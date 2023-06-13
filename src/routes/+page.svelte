754b69b9c6fe88ca2ab5c7e3c8db3587910b8f04
Steven R. Baker <steven@stevenrbaker.com>
2023-06-13 23:47:00 +0200

    Add a button to clear the choodle.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
     import { browser } from '$app/environment';
-
+    
     /* Configuration */
     const lineWidth = 5;
-
+    
     if (browser) {    
         const canvas: HTMLCanvasElement = document.getElementById('choodle-board')! as HTMLCanvasElement;
         const context = canvas.getContext('2d')!;
@@ -16,7 +16,7 @@
         
         let isDrawing = false;
         
-        const draw = (e) => {
+        const draw = (e: MouseEvent) => {
             if(!isDrawing) {
                 return;
             }
@@ -28,7 +28,7 @@
             context.stroke();
         }
         
-        canvas.addEventListener('mousedown', (e) => {
+        canvas.addEventListener('mousedown', e => {
             isDrawing = true;
         });
         
@@ -39,7 +39,26 @@
         });
         
         canvas.addEventListener('mousemove', draw);
+        
+        document.addEventListener('click', e => {
+            if (e.target.id === 'clear-board') {
+                const canvas: HTMLCanvasElement = document.getElementById('choodle-board')! as HTMLCanvasElement;
+                const context = canvas.getContext('2d')!;
+                
+                context.clearRect(0, 0, canvas.width, canvas.height);
+            }
+        });
     }
+    
+    const clear = () => {
+        const canvas: HTMLCanvasElement = document.getElementById('choodle-board')! as HTMLCanvasElement;
+        const context = canvas.getContext('2d')!;
+        
+        context.clearRect(0, 0, canvas.width, canvas.height);
+    }
+    
 </script>
 
 <canvas style="border: 1px solid red" id="choodle-board" />
+
+<button id="clear-board">Clear</button>
