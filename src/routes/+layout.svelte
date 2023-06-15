f1bb0cdbbaf7c8d83a4638c9376d7597e5a3be27
Brandon Dees <dees@pm.me>
2023-06-15 12:06:32 -0400

    set viewport meta the right way

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
-<head>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-</head>
+<svelte:head>
+    <title>Choodle Prototype</title>
+</svelte:head>
+
 <div>
 <slot></slot>
 </div>
