661e9e981ba2afd5e1457f3b1e329c5591fe4c5c
Brandon Dees <dees@pm.me>
2023-06-16 17:16:20 -0400

    fixup! start hooking up global styles and reshaping the button interface

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,37 +1,7 @@
-
-<style>
-    * {
-        box-sizing: border-box;
-    }
-
-    body, html {
-        margin: 0;
-        padding: 0;
-        overflow: hidden; /* Avoid scrollbars */
-        height: 100%; /* Make sure html and body cover full height */
-        width: 100%; /* Make sure html and body cover full width */
-    }
-
-    #choodle-board {
-        position: absolute; /* Absolute positioning removes any default margins/padding */
-        top: 0;
-        left: 0;
-        z-index: -1000;
-    }
-
-    #buttons {
-        z-index: 10000;
-    }
-</style>
-
-<div id="container">
-    <div id="drawing-area">
-        <canvas id="choodle-board" style="border: 1px red dashed"></canvas>
-    </div>
-    <div id="buttons">
-        <button id="clear-board">Clear</button>
-        <button id="undo">Undo</button>
-        <button id="save-board">Save</button>
-        <button id="load-board">Load</button>
-    </div>
+<div id="buttons">
+    <button id="clear-board">Clear</button>
+    <button id="undo">Undo</button>
+    <button id="save-board">Save</button>
+    <button id="load-board">Load</button>
 </div>
+<canvas id="choodle-board"></canvas>
