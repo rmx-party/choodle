25a77f03d9e48fccc05d61e84847bef0f778b967
Brandon Dees <dees@pm.me>
2023-06-16 17:14:56 -0400

    start hooking up global styles and reshaping the button interface

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,13 +2,19 @@
     <title>Choodle Prototype</title>
 </svelte:head>
 
-<div>
-<slot></slot>
-</div>
+<main id="main">
+    <noscript>
+        <p>
+            This app requires JavaScript to run. Please enable JavaScript and reload the page.
+        </p>
+    </noscript>
+    <slot />
+</main>
 
 <script lang="ts">
 import { dev } from "$app/environment"
 import { inject } from "@vercel/analytics"
+import '../app.css'
 
 inject({ mode: (dev ? 'development' : 'production')});
 </script>
