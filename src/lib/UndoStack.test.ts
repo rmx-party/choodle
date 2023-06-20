7cdf6562ba9e878f956151f152468618a30a247f
Steven R. Baker <steven@stevenrbaker.com>
2023-06-20 17:24:57 +0200

    Start adding UndoStack.

--- src/lib/UndoStack.test.ts
+++ src/lib/UndoStack.test.ts
@@ -1,20 +1,38 @@
 import {describe, expect, it} from 'vitest';
 
 class UndoStack {
-    private readonly stack: [] | [string]
+    private stack: [] | [string]
+    private cursor: number
 
     constructor() {
-        this.stack = []
+        this.stack = ['']
+        this.cursor = 0
     }
+
     get current(): string {
-        if (this.stack.length === 0) {
-            return ''
-        }
-        return this.stack[this.stack.length - 1]
+        return this.stack[this.cursor]
     }
 
     public push = (item: string) => {
         this.stack.push(item)
+        this.cursor += 1
+    }
+
+    public undo = () => {
+        if (this.cursor !== 0) {
+            this.cursor -= 1
+        }
+    }
+
+    public clear() {
+        this.stack = ['']
+        this.cursor = 0
+    }
+
+    public redo() {
+        if (this.cursor < this.stack.length - 1) {
+            this.cursor += 1
+        }
     }
 }
 
@@ -43,7 +61,95 @@ describe('UndoStack', () => {
         expect(undoStack.current).toBe('baz')
     });
 
-    describe('undo', () => {});
-    describe('redo', () => {});
-    describe('truncate', () => {});
+    describe('undo', () => {
+        it('gives the previous entry when undo is called once', () => {
+            const undoStack = new UndoStack()
+
+            undoStack.push('foo')
+            undoStack.push('bar')
+            undoStack.push('baz')
+
+            undoStack.undo()
+
+            expect(undoStack.current).toBe('bar')
+        });
+
+        it('gives the correct entry when undo is called twice', () => {
+            const undoStack = new UndoStack()
+
+            undoStack.push('foo')
+            undoStack.push('bar')
+            undoStack.push('baz')
+
+            undoStack.undo()
+            undoStack.undo()
+
+            expect(undoStack.current).toBe('foo')
+        });
+
+        it('makes current the first item when undoing past the beginning', () => {
+            const undoStack = new UndoStack()
+
+            undoStack.push('foo')
+
+            undoStack.undo()
+            undoStack.undo()
+
+            expect(undoStack.current).toBe('')
+        });
+    });
+
+    describe('redo', () => {
+        it('makes the previous entry current when undoing twice', () => {
+            const undoStack = new UndoStack()
+
+            undoStack.push('foo')
+            undoStack.push('bar')
+            undoStack.push('baz')
+
+            undoStack.undo()
+            undoStack.undo()
+            undoStack.redo()
+
+            expect(undoStack.current).toBe('bar')
+        });
+
+        it('makes the last entry current when redoing past the end', () => {
+            const undoStack = new UndoStack()
+
+            undoStack.push('foo')
+            undoStack.push('bar')
+            undoStack.push('baz')
+
+            undoStack.redo()
+
+            expect(undoStack.current).toBe('baz')
+        });
+    });
+
+    describe('truncate', () => {
+        it('truncates the undostack from current to end', () => {});
+    });
+
+    describe('push', () => {
+        it('when a new item is pushed in the middle of the stack, truncate the stack', () => {});
+    });
+
+    describe('clear', () => {
+        it('empties the stack', () => {
+            const undoStack = new UndoStack()
+
+            undoStack.push('foo')
+            undoStack.push('bar')
+            undoStack.push('baz')
+
+            undoStack.clear()
+
+            expect(undoStack.current).toBe('')
+        });
+    });
+
+    describe('storage', () => {
+        // pass in the storage to the constructor
+    });
 });
