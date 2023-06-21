7b2a800ec56482c31b7a9d571a19a47f9582ba2a
Steven R. Baker <steven@stevenrbaker.com>
2023-06-21 18:44:23 +0200

    Enabling Magic wallet login.

--- src/routes/login/+page.ts
+++ src/routes/login/+page.ts
@@ -1,6 +1,7 @@
 import {Magic} from "magic-sdk";
-import {PUBLIC_MAGIC_API_KEY} from '$env/static/public';
 import {browser} from "$app/environment";
+import {ethers} from "ethers";
+import {PUBLIC_MAGIC_API_KEY} from "$env/static/public";
 
 if (browser) {
     const magic = new Magic(PUBLIC_MAGIC_API_KEY, {
@@ -9,4 +10,11 @@ if (browser) {
             chainId: 80001
         }
     });
+
+    const provider = await magic.wallet.getProvider();
+    const web3Provider = new ethers.BrowserProvider(window.ethereum)
+    const accounts = await magic.wallet.connectWithUI();
+
+    const walletInfo = await magic.user.getInfo();
+    console.log(walletInfo);
 }
