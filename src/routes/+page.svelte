27770d4cf99ece0f0386f8a17a9f2a5d891eb591
Brandon Dees <dees@pm.me>
2023-06-21 09:45:42 -0500

    reorder buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <div id="buttons">
-    <button id="clear-board">Clear</button>
     <button id="undo">Undo</button>
     <button id="redo">Redo</button>
+    <button id="clear-board">Clear</button>
 </div>
 <canvas id="choodle-board"></canvas>
