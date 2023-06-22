fbbf544decc8c9f59c0ec7de8a76654c173d06dc
Brandon Dees <dees@pm.me>
2023-06-22 16:27:51 -0500

    lose the download

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
     <button id="undo" on:click={undo}>Undo</button>
     <button id="redo" on:click={redo}>Redo</button>
     <button id="clear-board" on:click={clear}>Clear</button>
-    <a id="download" href="/api/download">Download</a>
+    <!-- <a id="download" href="/api/download">Download</a> -->
     <!-- <button id="mint" on:click={mint}>Mint</button> -->
     {#if canShare()}
         <button id="share" on:click={share}>Share</button>
