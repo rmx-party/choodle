750e83aeabf4068aa25d6280df58e5ef7a615de7
Steven R. Baker <steven@stevenrbaker.com>
2023-06-27 22:22:39 +0200

    Try to estimate gas.

--- frontend/src/routes/login/+page.ts
+++ frontend/src/routes/login/+page.ts
@@ -2,6 +2,8 @@ import {Magic} from "magic-sdk";
 import {browser} from "$app/environment";
 import {ethers} from "ethers";
 import {PUBLIC_MAGIC_API_KEY} from "$env/static/public";
+import {Web3} from "web3";
+import abi from '../../abi/MyToken.json'
 
 async function setUpMagic() {
     const magic = new Magic(PUBLIC_MAGIC_API_KEY, {
@@ -21,6 +23,17 @@ async function setUpMagic() {
     await magic.wallet.showUI();
     // funds check
     // redirect to the minting modal
+
+    const web3 = new Web3(magic.rpcProvider)
+    const contractAddress = import.meta.env.VITE_CONTRACT_ADDRESS
+    console.log(`contract:`, contractAddress)
+    if (!(contractAddress?.length > 0)) {
+        throw new Error(`ENV var VITE_CONTRACT_ADDRESS not set`)
+    }
+    const contract = new web3.eth.Contract(abi, contractAddress)
+
+    const gasEstimation = await contract.methods.safeMint().estimateGas({from: accounts[0], value: 'foooobar'})
+    console.log('estimated gas: ', gasEstimation)
 }
 
 if (browser) {
