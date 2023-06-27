81169a93ec22837cd40721455418920470dbdb80
Steven R. Baker <steven@stevenrbaker.com>
2023-06-27 19:06:35 +0200

    Use a smaller image for testing.

--- test/MyToken.ts
+++ test/MyToken.ts
@@ -7,9 +7,12 @@ describe("MyToken", function () {
     it("Deployment should assign the total supply of tokens to the owner", async function () {
         const [owner, recipient1, recipient2] = await ethers.getSigners();
 
-        const mintMePlease = fs.readFileSync(path.join(__dirname, './scratch.base64'), {encoding: 'ascii'})
+        // const mintMePlease = fs.readFileSync(path.join(__dirname, './scratch.base64'), {encoding: 'ascii'})
+        const mintMePlease = fs.readFileSync(path.join(__dirname, './64x64-tnt.png.base64'), {encoding: 'ascii'})
 
         const myToken = await ethers.deployContract("MyToken");
+
+        console.log(myToken)
 
         console.log(await myToken.safeMint(recipient2, mintMePlease))
     });
