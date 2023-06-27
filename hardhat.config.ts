a4234eb538a11cc191667261ea5c8ca9486c450a
Steven R. Baker <steven@stevenrbaker.com>
2023-06-27 19:06:25 +0200

    Add the gas reporter plugin, and turn on optimizations.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,24 @@
 import {HardhatUserConfig} from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+import "hardhat-gas-reporter"
+
 const config: HardhatUserConfig = {
-    solidity: "0.8.19",
+    solidity: {
+        version: "0.8.19",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 1000,
+            },
+        },
+    },
+    gasReporter: {
+        enabled: true,
+        currency: 'USD',
+        gasPrice: 28,
+        coinmarketcap: 'API HERE'
+    },
 };
 
 export default config;
