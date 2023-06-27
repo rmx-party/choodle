9945bd871f147e88099d53d39a7a0cd0100982c1
Steven R. Baker <steven@stevenrbaker.com>
2023-06-27 22:28:32 +0200

    Add deployment script.

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,27 +1,17 @@
-import { ethers } from "hardhat";
+import hre from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+    const NFT = await hre.ethers.getContractFactory("MyToken");
+    const nft = await NFT.deploy();
 
-  const lockedAmount = ethers.parseEther("0.001");
+    nft.deploymentTransaction();
 
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+    console.log("deployed to:", nft.getAddress());
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+    console.error(error);
+    process.exitCode = 1;
 });
