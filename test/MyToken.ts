8dd1d8af5ee1ce2e1a5cd8479a3946815592edf3
Steven R. Baker <steven@stevenrbaker.com>
2023-06-27 20:54:57 +0200

    Complete the happy path test for minting a small image.

--- test/MyToken.ts
+++ test/MyToken.ts
@@ -1,19 +1,17 @@
 import {ethers} from "hardhat";
+import {expect} from "chai";
 
 const fs = require('fs');
 const path = require('path')
 
 describe("MyToken", function () {
-    it("Deployment should assign the total supply of tokens to the owner", async function () {
+    it("doesn't fail when saving a small image", async function () {
         const [owner, recipient1, recipient2] = await ethers.getSigners();
 
-        // const mintMePlease = fs.readFileSync(path.join(__dirname, './scratch.base64'), {encoding: 'ascii'})
         const mintMePlease = fs.readFileSync(path.join(__dirname, './64x64-tnt.png.base64'), {encoding: 'ascii'})
 
         const myToken = await ethers.deployContract("MyToken");
 
-        console.log(myToken)
-
-        console.log(await myToken.safeMint(recipient2, mintMePlease))
+        expect(await myToken.safeMint(recipient2, mintMePlease)).not.Throw
     });
 });
