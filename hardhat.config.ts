6c388d811e95aefc3c6975fe6d5b4664e0e92273
Steven R. Baker <steven@stevenrbaker.com>
2023-06-27 17:54:32 +0200

    Make the configured solidity version match the version in the contract.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,8 @@
-import { HardhatUserConfig } from "hardhat/config";
+import {HardhatUserConfig} from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+    solidity: "0.8.19",
 };
 
 export default config;
