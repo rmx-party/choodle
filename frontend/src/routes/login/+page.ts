7ca14121713e933af315af7a681806701d15ce7a
Steven R. Baker <steven@stevenrbaker.com>
2023-06-27 23:43:12 +0200

    Try to use ethers to estimate gas.

--- frontend/src/routes/login/+page.ts
+++ frontend/src/routes/login/+page.ts
@@ -1,8 +1,7 @@
 import {Magic} from "magic-sdk";
 import {browser} from "$app/environment";
 import {ethers} from "ethers";
-import {PUBLIC_MAGIC_API_KEY} from "$env/static/public";
-import {Web3} from "web3";
+import {PUBLIC_CONTRACT_ADDRESS, PUBLIC_MAGIC_API_KEY} from "$env/static/public";
 import abi from '../../abi/MyToken.json'
 
 async function setUpMagic() {
@@ -24,15 +23,15 @@ async function setUpMagic() {
     // funds check
     // redirect to the minting modal
 
-    const web3 = new Web3(magic.rpcProvider)
-    const contractAddress = import.meta.env.VITE_CONTRACT_ADDRESS
+    const web3 = new ethers.JsonRpcProvider('https://matic-mumbai.chainstacklabs.com')
+    const contractAddress = PUBLIC_CONTRACT_ADDRESS
     console.log(`contract:`, contractAddress)
     if (!(contractAddress?.length > 0)) {
         throw new Error(`ENV var VITE_CONTRACT_ADDRESS not set`)
     }
-    const contract = new web3.eth.Contract(abi, contractAddress)
-
-    const gasEstimation = await contract.methods.safeMint().estimateGas({from: accounts[0], value: 'foooobar'})
+    const contract = new ethers.Contract(accounts[0], abi, web3Provider)
+    const gasEstimation = await
+        contract.safeMint.estimateGas(accounts[0], 'foooobar')
     console.log('estimated gas: ', gasEstimation)
 }
 
