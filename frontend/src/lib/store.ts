6cee507dd1abb0e02bdc54d047c09bee80a92fff
Steven R. Baker <steven@stevenrbaker.com>
2023-06-30 18:57:55 +0200

    Ship a loading screen.

--- frontend/src/lib/store.ts
+++ frontend/src/lib/store.ts
@@ -1,3 +1,4 @@
 import {writable} from 'svelte/store';
 
 export const tokenId = writable(null);
+export const loading = writable(false);
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
-    import {tokenId} from "$lib/store";
+    import {loading, tokenId} from "$lib/store";
 
     let tokenIdValue;
+    let loadingValue;
 
     tokenId.subscribe(value => {
         tokenIdValue = value
+    })
+
+    loading.subscribe(value => {
+        loadingValue = value
     })
 
     import {
@@ -51,7 +56,7 @@
 
     export const mint = async (event: Event) => {
         event.preventDefault()
-
+        loadingValue = true
         tokenIdValue = await connectAndMint()
         await goto(`/viewToken/${tokenIdValue}`)
     }
@@ -84,14 +89,62 @@
     initialize()
 </script>
 
-<div id="buttons">
-    <button id="undo" on:click={undo}>Undo</button>
-    <button id="redo" on:click={redo}>Redo</button>
-    <button id="clear-board" on:click={clear}>Clear</button>
-    <!-- <a id="download" href="/api/download">Download</a> -->
-    <button id="mint" on:click={mint}>Mint</button>
-    {#if canShare()}
-        <button id="share" on:click={share}>Share</button>
-    {/if}
-</div>
-<canvas id="choodle-board"></canvas>
+{#if !loadingValue}
+    <div id="buttons">
+        <button id="undo" on:click={undo}>Undo</button>
+        <button id="redo" on:click={redo}>Redo</button>
+        <button id="clear-board" on:click={clear}>Clear</button>
+        <!-- <a id="download" href="/api/download">Download</a> -->
+        <button id="mint" on:click={mint}>Mint</button>
+        {#if canShare()}
+            <button id="share" on:click={share}>Share</button>
+        {/if}
+    </div>
+    <canvas id="choodle-board"></canvas>
+{:else}
+    <style>
+        :root {
+            --light-color: rgba(10, 10, 220, .2);
+            --dark-color: rgba(10, 10, 220, 1);
+            --radius: 64px;
+            --ring-width: 4px;
+        }
+
+        /* loading element style */
+        .loading {
+            margin: 0;
+            padding: 0;
+            width: var(--radius);
+            height: var(--radius);
+            border-radius: 50%;
+            border: var(--ring-width) solid var(--light-color);
+        }
+
+        .loading:before {
+            margin: 0;
+            padding: 0;
+            display: block;
+            position: relative;
+            left: calc(var(--ring-width) * -2);
+            top: calc(var(--ring-width) * -2);
+            content: ' ';
+            width: var(--radius);
+            height: var(--radius);
+            border-radius: 50%;
+            border: var(--ring-width) solid;
+            border-color: var(--dark-color) transparent transparent transparent;
+            animation: loading-rotate .8s ease-out infinite;
+        }
+
+        @keyframes loading-rotate {
+            0% {
+                transform: rotate(0deg);
+            }
+            100% {
+                transform: rotate(360deg);
+            }
+        }
+    </style>
+
+    <div id="loading" class="loading"></div>
+{/if}
