35ac1cbae1348a61cc83c464a05b1b3e3e8090a2
Brandon Dees <dees@pm.me>
2023-07-10 18:11:49 -0500

    toying more with jpeg and webp lossy compression, committed for posterity

--- frontend/src/lib/CanvasStuff.ts
+++ frontend/src/lib/CanvasStuff.ts
@@ -4,7 +4,7 @@ import {UndoStack} from "$lib/UndoStack";
 import {goto} from "$app/navigation";
 
 /* Configuration */
-const lineWidth = 1;
+const lineWidth = 5;
 
 let isDrawing = false;
 
@@ -47,6 +47,7 @@ export function endDrawing(context: CanvasRenderingContext2D) {
 
 const drawTo = (x: number, y: number): void => {
     const context = canvasContext()
+    context.imageSmoothingEnabled = false;
 
     console.log(`drawing a line to ${x} ${y}`)
 
@@ -121,7 +122,9 @@ export async function getUndoStack() {
 export const push = async () => {
     const undoStack = await getUndoStack()
 
-    undoStack.push(canvas().toDataURL())
+    // undoStack.push(canvas().toDataURL())
+    // undoStack.push(canvas().toDataURL('image/jpeg', 0.7))
+    undoStack.push(canvas().toDataURL('image/webp', 0.01))
 
     await setUndoStack(undoStack);
     logState()
@@ -130,6 +133,10 @@ export const push = async () => {
 export function pixelRatio(): number {
     // return window.devicePixelRatio;
     return 1/4;
+    // return 1/3;
+    // return 1/2;
+    // return 2/3;
+    // return 1;
 }
 
 export const resizeCanvas = async (_event?: Event) => {
@@ -141,12 +148,15 @@ export const resizeCanvas = async (_event?: Event) => {
 
     const bounds = canvas().getBoundingClientRect();
 
+    // const targetMaxSize = {x: 100, y: 200}
     // const targetMaxSize = {x: 215, y: 466}
     const targetMaxSize = {x: 430, y: 932}
     // const targetMaxSize = {x: 860, y: 1864}
     // const targetMaxSize = {x: 1290, y: 2796}
-    canvas().width = (Math.min(windowWidth, targetMaxSize.x) - bounds.x) * ratio;
-    canvas().height = (Math.min(windowHeight, targetMaxSize.y) - bounds.y) * ratio;
+    // canvas().width = (Math.min(windowWidth, targetMaxSize.x) - bounds.x) * ratio;
+    // canvas().height = (Math.min(windowHeight, targetMaxSize.y) - bounds.y) * ratio;
+    canvas().width = Math.floor(targetMaxSize.x * ratio)
+    canvas().height = Math.floor(targetMaxSize.y * ratio)
     await load()
 }
 
