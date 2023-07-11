b345bb58259d1b650456c546a20c9738eefddaf3
Steven R. Baker <steven@stevenrbaker.com>
2023-07-11 21:12:12 +0200

    Remove unused code.

--- frontend/src/lib/ImageUtils.ts
+++ frontend/src/lib/ImageUtils.ts
@@ -1,26 +1,6 @@
 import {PNG} from 'pngjs/browser';
 import {browser} from '$app/environment';
 
-// FIXME: use pngjs to load the image, set some options, and create a new image
-
-export const shrinkImage = async (dataURI: string) => {
-    const options = {
-        colorType: 0,
-        inputHasAlpha: true,
-        deflateChunkSize: 256,
-        filterType: 0,
-    };
-
-    // const ab = await fetch(dataURI).then((response) => response.arrayBuffer())
-    const ab = await (await fetch(dataURI)).arrayBuffer()
-    console.log(ab.byteLength)
-
-    const png = await new PNG(options).parse(ab)
-    console.log(png.byteLength)
-
-    return png
-}
-
 export const crunchCanvas = async (canvas, ctx) => {
     const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
 
@@ -29,7 +9,7 @@ export const crunchCanvas = async (canvas, ctx) => {
         height: imageData.height,
         filterType: -1, // no filtering
         colorTYpe: 0,
-        bgColor: { red: 255, green: 255, blue: 255}
+        bgColor: {red: 255, green: 255, blue: 255}
     });
 
     const [threshold, redCoefficient, greenCoefficient, blueCoefficient] = [
@@ -43,13 +23,13 @@ export const crunchCanvas = async (canvas, ctx) => {
             const idx = (png.width * y + x) << 2;
 
             // Optimize the PNG by reducing color space to black and white
-            let grayscale = redCoefficient*imageData.data[idx] + greenCoefficient*imageData.data[idx+1] + blueCoefficient*imageData.data[idx+2];
+            let grayscale = redCoefficient * imageData.data[idx] + greenCoefficient * imageData.data[idx + 1] + blueCoefficient * imageData.data[idx + 2];
             grayscale = (grayscale > threshold) ? 255 : 0; // threshold, could be adjusted
 
             png.data[idx] = grayscale;   // red
-            png.data[idx+1] = grayscale; // green
-            png.data[idx+2] = grayscale; // blue
-            png.data[idx+3] = imageData.data[idx+3]; // alpha
+            png.data[idx + 1] = grayscale; // green
+            png.data[idx + 2] = grayscale; // blue
+            png.data[idx + 3] = imageData.data[idx + 3]; // alpha
         }
     }
 
@@ -60,7 +40,17 @@ export const crunchCanvas = async (canvas, ctx) => {
     const bufferSize = buffer.length;
     const dataUrlSize = dataUrlResult.length;
     const imageDataSize = imageData.data.length;
-    console.log(`crunched: `, { canvas, ctx, imageData, imageDataSize, png, buffer, bufferSize, dataUrlResult, dataUrlSize });
+    console.log(`crunched: `, {
+        canvas,
+        ctx,
+        imageData,
+        imageDataSize,
+        png,
+        buffer,
+        bufferSize,
+        dataUrlResult,
+        dataUrlSize
+    });
 
     return dataUrlResult;
 }
