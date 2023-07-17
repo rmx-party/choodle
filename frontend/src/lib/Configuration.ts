7e18bea775c6a9bd9e68348619cfda752a5fb30a
Brandon Dees <dees@pm.me>
2023-07-17 17:38:57 -0500

    bump up the pixel ratio a lil more with the new settings keeps well under our limit

--- frontend/src/lib/Configuration.ts
+++ frontend/src/lib/Configuration.ts
@@ -2,7 +2,7 @@ import type {Dimensiony} from "$lib/Calculations";
 
 export const lineWidth = 4;
 export const targetMaxSize: Dimensiony = {x: 420, y: 746}
-export const pixelRatio = 0.35
+export const pixelRatio = 0.4
 export const backgroundColour = "#ffffff"
 export const choodleUndoKey = 'choodle-undo'
 export const blackWhiteThreshold = 128;
