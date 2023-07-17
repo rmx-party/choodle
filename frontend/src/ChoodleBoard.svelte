8b2f08dac12ebb2309185183b6775a0a15d9bb2e
Steven R. Baker <steven@stevenrbaker.com>
2023-07-17 21:23:27 +0200

    Merge the buttons because they depend on the canvas; remove the rest of CanvasStuff.

--- frontend/src/ChoodleBoard.svelte
+++ frontend/src/ChoodleBoard.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
     import {browser} from "$app/environment";
-    import {getUndoStack, setUndoStack} from "$lib/StorageStuff";
+    import {clearStorage, getUndoStack, setUndoStack} from "$lib/StorageStuff";
     import {onMount} from "svelte";
-    import {lineWidth, pixelRatio, targetMaxSize} from "$lib/Configuration";
+    import {backgroundColour, lineWidth, pixelRatio, targetMaxSize} from "$lib/Configuration";
     import {applyRatio, maximumSize, removeOffset} from "$lib/Calculations";
-    import ChoodleBoardButtons from "./ChoodleBoardButtons.svelte";
-    import {drawImageFromDataURL} from "$lib/CanvasStuff";
     import {crunchCanvas} from "$lib/ImageUtils";
+    import {loading, tokenId} from "$lib/store";
+    import {connectAndMint} from "$lib/MagicStuff";
+    import {goto} from "$app/navigation";
 
     export let id;
 
@@ -112,6 +113,94 @@
         await setUndoStack(undoStack);
     }
 
+    function canShare(): boolean {
+        if (browser) return !!navigator.share
+        return false
+    }
+
+    const undo = async (event: Event) => {
+        event.preventDefault()
+
+        const undoStack = await getUndoStack()
+        undoStack.undo()
+
+        await setUndoStack(undoStack);
+
+        const dataURL = undoStack.current
+
+        drawImageFromDataURL(dataURL, ctx)
+    }
+
+    const redo = async (event: Event) => {
+        event.preventDefault()
+
+        const undoStack = await getUndoStack()
+        undoStack.redo()
+
+        await setUndoStack(undoStack);
+
+        drawImageFromDataURL(undoStack.current, ctx)
+    }
+
+    const clear = async (event: Event) => {
+        event.preventDefault()
+
+        clearCanvas('choodle-board');  // FIXME: this should not be hard-coded
+        await clearStorage();
+    }
+
+    const mint = async (event: Event) => {
+        event.preventDefault()
+        loading.set(true)
+        tokenId.set(await connectAndMint())
+        await goto(`/viewToken/${$tokenId}`)
+    }
+
+    const share = async (event: Event) => {
+        event.preventDefault()
+
+        const imgBlob = await (await fetch(canvas.toDataURL("image/png", 1.0))).blob();
+        const files = [
+            new File(
+                [imgBlob],
+                'choodle.png',
+                {
+                    type: 'image/png',
+                    lastModified: Date.now()
+                }
+            )
+        ];
+        if (navigator.share) {
+            navigator.share({
+                files
+            }).then(() => {
+                console.log('Thanks for sharing!');
+            }).catch(console.error);
+        } else {
+            console.error('Web Share API not supported')
+        }
+    };
+
+    function clearCanvas(id: string) {
+        const canvas = document.getElementById(id) as HTMLCanvasElement;
+        const ctx = canvas.getContext('2d')!
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle = backgroundColour;
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        ctx.imageSmoothingEnabled = false;
+    }
+
+    function drawImageFromDataURL(dataURL: string, context: CanvasRenderingContext2D) {
+        const image = new Image;
+        clearCanvas('choodle-board') // FIXME: this should not be hard-coded
+        image.addEventListener('load', () => {
+            context.drawImage(image, 0, 0);
+            context.stroke();
+        });
+        image.src = dataURL;
+    }
+
     onMount(async () => {
         if (!browser) return;
 
@@ -129,7 +218,16 @@
     });
 </script>
 
-<ChoodleBoardButtons choodleBoardId={id}></ChoodleBoardButtons>
+<div id="buttons">
+    <button id="undo" on:click={undo}>Undo</button>
+    <button id="redo" on:click={redo}>Redo</button>
+    <button id="clear-board" on:click={clear}>Clear</button>
+    <!-- <a id="download" href="/api/download">Download</a> -->
+    <button id="mint" on:click={mint}>Mint</button>
+    {#if canShare()}
+        <button id="share" on:click={share}>Share</button>
+    {/if}
+</div>
 <canvas id={id} style="border: 1px solid lawngreen"
         on:mousedown={startDrawing}
         on:touchstart={startDrawing}
