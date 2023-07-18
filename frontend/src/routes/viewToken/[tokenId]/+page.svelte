06105707eb1bf075f540cc9316a70050baf24c15
Steven R. Baker <steven@stevenrbaker.com>
2023-07-18 22:00:29 +0200

    Now actually navigate between the index and show actions.

--- frontend/src/routes/viewToken/[tokenId]/+page.svelte
+++ frontend/src/routes/viewToken/[tokenId]/+page.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
     import {urlFor} from "$lib/PersistedImagesUtils"
 
-    export let data = [];
+    export let data = {};
 </script>
 
 {#if data.choodle }
-    <h1>{data.choodle[0].title}</h1>
-    <img src={urlFor(data.choodle[0].image)}/>
+    <h1>{data.choodle.title}</h1>
+    <img src={urlFor(data.choodle.image)}/>
 {:else}
     <p>No choodle found.</p>
 {/if}
--- frontend/src/routes/viewToken/[tokenId]/+page.ts
+++ frontend/src/routes/viewToken/[tokenId]/+page.ts
@@ -1,11 +1,11 @@
 import {client} from "$lib/PersistedImagesUtils";
 
 export async function load({params}) {
-    const data = await client.fetch(`*[_type == "choodle"]`);
+    const data = await client.fetch(`*[_type == "choodle" && _id == "${params.tokenId}"]`);
 
     if (data) {
         return {
-            choodle: data
+            choodle: data[0]
         };
     }
     return {
