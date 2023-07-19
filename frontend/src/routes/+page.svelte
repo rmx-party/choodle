b12576191b57b5536404027755d1644d5407d9a8
Brandon Dees <dees@pm.me>
2023-07-19 16:22:51 -0500

    logo on welcome page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,3 +1,21 @@
 
-<h1>Welcome to Choodle</h1>
-<a href="/draw">do a choodle</a>
+<script>
+import wordmark from '$lib/assets/choodle-wordmark.png';
+</script>
+<h1>Welcome to 
+    <img alt="Choodle" src={wordmark} width="300"/>
+</h1>
+<a href="/draw">Let's Play!</a>
+
+<style>
+    :root {
+        text-align: center;
+    }
+    h1 {
+        line-height: 3em;
+    }
+    h1 img {
+        display: inline-block;
+        font-size: 4rem;
+    }
+</style>
