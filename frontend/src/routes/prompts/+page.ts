ecef00314d153a524ac0aaa48f82987d7d327e7c
Brandon Dees <dees@pm.me>
2023-07-19 13:57:43 -0500

    only published prompts, ordered by creation date

--- frontend/src/routes/prompts/+page.ts
+++ frontend/src/routes/prompts/+page.ts
@@ -3,12 +3,13 @@ import {createClient} from '@sanity/client';
 const client = createClient({
     projectId: "tdnjp9se",
     dataset: "production",
+    perspective: 'published',
     apiVersion: "2023-07-18",
     useCdn: false
 })
 
 export async function load({params}) {
-    const data = await client.fetch(`*[_type == "dailyPrompt"]`);
+    const data = await client.fetch(`*[_type == "dailyPrompt"] | order(_createdAt)`);
 
     if (data) {
         console.log(`load data: `, data)
