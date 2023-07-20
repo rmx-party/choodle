965eecc8c861eb90f3e9da32363baedb533d9731
Brandon Dees <dees@pm.me>
2023-07-20 10:09:16 -0500

    restore transparency on prompt overlay

--- frontend/src/Prompt.svelte
+++ frontend/src/Prompt.svelte
@@ -6,10 +6,12 @@
 
 {#if prompt}
     <div class="anchor">
-        <p class="daily-prompt board-overlay">
-            <img src={wordmark} alt="Choodle" height="16" width="55" /><br/>
-            <span>{prompt.prompt}</span>
-        </p>
+        <div class="board-overlay">
+            <p class="daily-prompt">
+                <img src={wordmark} alt="Choodle" height="16" width="55" /><br/>
+                <span>{prompt.prompt}</span>
+            </p>
+        </div>
     </div>
 {:else}
     <span style="display:none;">no prompt</span>
@@ -25,21 +27,23 @@
         height: 0;
     }
     .board-overlay {
-        border-radius: 1.5rem;
-        background: var(--color-primary);
-        /* box-shadow: 0px 0px 20px 10px hsla(0, 0%, 0%, 0.1); */
-        box-shadow: 0px 4px 24px 0px rgba(0, 0, 0, 0.01), 0px -2px 8px 0px rgba(0, 0, 0, 0.12) inset, 0px 2px 24px 0px rgba(255, 255, 255, 0.40) inset, 0px 1px 4px 0px rgba(0, 0, 0, 0.04);
+        opacity: 90%;
         z-index: 999;
         position: absolute;
         display: block;
         margin: 0 auto;
+        padding: 0;
         top: 1em;
         left: 1em;
         right: 1em;
     }
     .daily-prompt {
+        border-radius: 1.5rem;
+        background: var(--color-primary);
+        box-shadow: 0px 4px 24px 0px rgba(0, 0, 0, 0.01), 0px -2px 8px 0px rgba(0, 0, 0, 0.12) inset, 0px 2px 24px 0px rgba(255, 255, 255, 0.40) inset, 0px 1px 4px 0px rgba(0, 0, 0, 0.04);
         font-weight: 700;
         padding: 0.5em;
+        margin: 0;
         pointer-events: none;
         text-align: center;
         vertical-align: bottom;
