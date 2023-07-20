92bfe60263294e4cf950dc510e6d9ba6beee7f31
Steven R. Baker <steven@stevenrbaker.com>
2023-07-20 18:48:58 +0200

    Add the OG metadata back.

--- frontend/src/routes/choodle/[id]/+page.svelte
+++ frontend/src/routes/choodle/[id]/+page.svelte
@@ -1,8 +1,25 @@
 <script lang="ts">
     import {urlFor} from "$lib/PersistedImagesUtils"
+    import {page} from "$app/stores";
 
     export let data = {};
 </script>
+
+<svelte:head>
+    <!-- TODO: set canonical URL (decide trailing slash etc) -->
+    <meta property="og:type" content='website'/>
+    <meta property="og:url" content={$page.url}/>
+    <meta property="og:site_name" content="Choodle"/>
+    <title>{data.choodle.title}</title>
+    <meta property="og:title" content={data.choodle.title}/>
+    <meta property="og:image" content={urlFor(data.choodle.image)}/>
+    <meta property="og:image:secure_url" content={urlFor(data.choodle.image)}/>
+    <meta property="og:image:width" content="430"/>
+    <meta property="og:image:height" content="932"/>
+    <meta property="og:image:alt" content={data.choodle.title}/>
+    <meta property="og:description" content={data.choodle.title}/>
+    <meta name="description" content={data.choodle.title}/>
+</svelte:head>
 
 {#if data.choodle }
     <h1>{data.choodle.title}</h1>
