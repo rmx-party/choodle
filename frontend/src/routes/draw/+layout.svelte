5a33a0f5ae79084d5e4bb522465dfaed3811dc3b
Steven R. Baker <steven@stevenrbaker.com>
2023-07-20 20:21:41 +0200

    Fix scrolling on the non-drawing pages.

--- frontend/src/routes/draw/+layout.svelte
+++ frontend/src/routes/draw/+layout.svelte
@@ -4,11 +4,26 @@
           content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, initial-scale=1.0, user-scalable=no">
 </svelte:head>
 
-<div id="draw">
-    <slot/>
+<div id="container">
+    <div id="draw">
+        <slot/>
+    </div>
 </div>
 
 <style>
+    #container {
+        position: absolute;
+        overflow: hidden; /* Avoid scrollbars */
+        margin: 0;
+        padding: 0;
+        width: 100vw;
+        height: 100vh;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+    }
+
     #draw {
         height: 100vh;
         width: 100vw;
--- frontend/src/routes/draw/+page.svelte
+++ frontend/src/routes/draw/+page.svelte
@@ -17,21 +17,8 @@
           height="630"
           description="Draw something that lasts forever. Express yourself with lo-fi doodles that are yours to keep, sell, or share."/>
 
-<div id="container">
-    {#if !$loading}
-        <ChoodleBoard id="choodle-board" prompt={data.prompt}></ChoodleBoard>
-    {:else}
-        <LoadingIndicator></LoadingIndicator>
-    {/if}
-</div>
-
-<style>
-    #container {
-        position: absolute;
-        overflow: hidden; /* Avoid scrollbars */
-        margin: 0;
-        padding: 0;
-        width: 100%;
-        height: 100%;
-    }
-</style>
+{#if !$loading}
+    <ChoodleBoard id="choodle-board" prompt={data.prompt}></ChoodleBoard>
+{:else}
+    <LoadingIndicator></LoadingIndicator>
+{/if}
