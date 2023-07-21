5f2d9a0f2d0740be10ec6db246dd1e1ec8cb9434
Brandon Dees <dees@pm.me>
2023-07-21 11:12:29 -0500

    introduce dejavu sans webfonts

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -12,6 +12,7 @@
     import {dev} from "$app/environment"
     import {inject} from "@vercel/analytics"
     import '../app.css'
+    import '../fonts.css'
 
     inject({mode: (dev ? 'development' : 'production')});
 </script>
