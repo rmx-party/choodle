67a50ceefb1ebee48b4b4221f389039d65a45d1a
Steven R. Baker <steven@stevenrbaker.com>
2023-07-21 18:36:37 +0200

    Sanity requires environment variables to be named in a certain way.

--- choodle-cms/sanity.config.ts
+++ choodle-cms/sanity.config.ts
@@ -7,8 +7,8 @@ export default defineConfig({
     name: 'default',
     title: 'Choodle CMS',
 
-    projectId: process.env.PUBLIC_SANITY_PROJECT_ID!,
-    dataset: process.env.PUBLIC_SANITY_DATASET!,
+    projectId: process.env.SANITY_STUDIO_PROJECT_ID!,
+    dataset: process.env.SANITY_STUDIO_DATASET!,
 
     plugins: [deskTool(), visionTool()],
 
