787943f84732a9affba95b10cf43817c55c68735
Steven R. Baker <steven@stevenrbaker.com>
2023-07-21 18:57:15 +0200

    Add a datatype for howto.

---
+++ choodle-cms/schemas/howto.ts
@@ -0,0 +1,12 @@
+export default {
+    name: 'howto',
+    type: 'document',
+    title: 'Howto',
+    fields: [
+        {
+            name: 'howto',
+            type: 'string',
+            title: 'Howto'
+        }
+    ]
+}
--- choodle-cms/schemas/index.ts
+++ choodle-cms/schemas/index.ts
@@ -1,7 +1,9 @@
 import dailyPrompt from './dailyPrompt';
 import choodle from "./choodle";
+import howto from "./howto";
 
 export const schemaTypes = [
     dailyPrompt,
-    choodle
+    choodle,
+    howto
 ]
