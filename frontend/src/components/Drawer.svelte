a4a206376f2e3d9ace92ffa9a4e887b178104552
Brandon Dees <dees@pm.me>
2023-07-24 13:20:49 -0500

    more drawer sketching

--- frontend/src/components/Drawer.svelte
+++ frontend/src/components/Drawer.svelte
@@ -19,13 +19,13 @@
 <div id="drawer" class={toggleState}>
     <section class="drawer-content">
         {@html content}
+        <hr/>
     </section>
     <section class="drawer-prompt">
-        <hr/>
         {prompt}
     </section>
-    <button on:click|preventDefault={handleTap}>
-        toggle me
+    <button class="drag-zone" on:click|preventDefault={handleTap}>
+        <div class="drawer-pull">pull</div>
     </button>
 </div>
 
@@ -37,6 +37,9 @@
         z-index: 99999;
 
         background-color: var(--color-primary);
+        text-align: center;
+        border-radius: 0 0 1rem 1rem;
+        padding: 1rem 1rem 0;
     }
 
     .drawer-content {
@@ -52,5 +55,31 @@
     .minimized .drawer-prompt {
         height: 0;
         overflow: hidden;
+        text-align: center;
+    }
+
+    .drag-zone {
+        height: 1.5rem;
+        width: 100%;
+        background: none;
+        border: none;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    .drawer-pull {
+        border-radius: 0.25rem;
+        width: 3rem;
+        height: 0.25rem;
+        border: none;
+        outline: none;
+        background: rgba(8, 11, 9, 0.88);
+        overflow: hidden;
+    }
+
+    hr {
+        border-top: 1px;
+        border-color: rgba(0, 0, 0, 0.10);
+        width: 100%;
     }
 </style>
