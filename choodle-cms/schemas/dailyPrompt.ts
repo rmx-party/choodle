6cf6995e86086facd4f6b1671c2d0e917d1e0ee6
Steven R. Baker <steven@stevenrbaker.com>
2023-07-24 21:18:32 +0200

    Prompts should have a maximum length of 14 characters.

--- choodle-cms/schemas/dailyPrompt.ts
+++ choodle-cms/schemas/dailyPrompt.ts
@@ -1,13 +1,14 @@
 export default {
-  name: 'dailyPrompt',
-  type: 'document',
-  title: 'Daily Prompt',
-  fields: [
-    {
-      name: 'prompt',
-      type: 'string',
-      title: 'Prompt'
-    }
+    name: 'dailyPrompt',
+    type: 'document',
+    title: 'Daily Prompt',
+    fields: [
+        {
+            name: 'prompt',
+            type: 'string',
+            title: 'Prompt',
+            validation: Rule => Rule.required().max(14)
+        }
 
-  ]
+    ]
 }
