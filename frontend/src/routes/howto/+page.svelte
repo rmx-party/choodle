15ee9c485e01afc7adb5e23d11c451d5e0cf409d
Steven R. Baker <steven@stevenrbaker.com>
2023-07-28 21:55:00 +0200

    Revert "Remove the howto."
    This reverts commit 54d5b4dc96d6626d4c247217e67b6e3868ccb12c.

---
+++ frontend/src/routes/howto/+page.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+    import { goto } from '$app/navigation';
+    import Button from '../../components/Button.svelte';
+    import MetaData from '../../components/MetaData.svelte';
+    import Wordmark from "../../components/Wordmark.svelte";
+    import {toHTML} from "@portabletext/to-html";
+
+    export let data;
+
+    console.log(data)
+</script>
+
+
+<MetaData 
+    title="How to Choodle"
+    themeColor="#FEF40A"
+/>
+
+<main id="main">
+
+    <h1>
+        How to <Wordmark />
+    </h1>
+
+    <content id="content">
+        {@html toHTML(data.howto.howto)}
+    </content>
+
+    <Button variant="primary" on:click={() => {goto('/draw')}}>
+        Let's Draw!
+    </Button>
+</main>
+
+<style>
+    main {
+        background: var(--color-primary);
+        padding: 3rem;
+        margin: 0;
+        min-width: 100vw;
+        min-height: 100vh;
+        text-align: center;
+    }
+
+    .cta {
+        display: flex;
+        padding: 1em;
+        text-decoration: none;
+    }
+</style>
+
+
