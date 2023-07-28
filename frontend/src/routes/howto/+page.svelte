8b48163578d7990aed04ed8f83d883c670b9e472
Steven R. Baker <steven@stevenrbaker.com>
2023-07-28 21:57:36 +0200

    Unused css class, and some formatting.

--- frontend/src/routes/howto/+page.svelte
+++ frontend/src/routes/howto/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { goto } from '$app/navigation';
+    import {goto} from '$app/navigation';
     import Button from '../../components/Button.svelte';
     import MetaData from '../../components/MetaData.svelte';
     import Wordmark from "../../components/Wordmark.svelte";
@@ -11,15 +11,16 @@
 </script>
 
 
-<MetaData 
-    title="How to Choodle"
-    themeColor="#FEF40A"
+<MetaData
+        title="How to Choodle"
+        themeColor="#FEF40A"
 />
 
 <main id="main">
 
     <h1>
-        How to <Wordmark />
+        How to
+        <Wordmark/>
     </h1>
 
     <content id="content">
@@ -39,12 +40,6 @@
         min-width: 100vw;
         min-height: 100vh;
         text-align: center;
-    }
-
-    .cta {
-        display: flex;
-        padding: 1em;
-        text-decoration: none;
     }
 </style>
 
