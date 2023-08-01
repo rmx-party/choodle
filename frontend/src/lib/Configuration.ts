575858072adcf4b51c945674ec558eaa49c8a764
Steven R. Baker <steven@stevenrbaker.com>
2023-08-01 22:13:33 +0200

    Change the aspect ratio to 3:4

--- frontend/src/lib/Configuration.ts
+++ frontend/src/lib/Configuration.ts
@@ -1,7 +1,7 @@
 import type {Dimensiony} from "$lib/Calculations";
 
 export const lineWidth = 1;
-export const targetMaxSize: Dimensiony = {x: 168, y: 298}
+export const targetMaxSize: Dimensiony = {x: 195, y: 260}
 export const drawColor = "#000000"
 export const backgroundColour = "#ffffff"
 export const choodleUndoKey = 'choodle-undo'
