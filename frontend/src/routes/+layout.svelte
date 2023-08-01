fde38fe7c160b71ef36cb7f6fd3f6b2b93586141
Steven R. Baker <steven@stevenrbaker.com>
2023-08-01 23:42:35 +0200

    Actually set up Bugsnag.

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,12 +1,6 @@
 <svelte:head>
     <meta name="viewport"
           content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, initial-scale=1.0">
-    <script src="//d2wy8f7a9ursnm.cloudfront.net/v7/bugsnag.min.js"></script>
-    <script>
-        import Bugsnag from "@bugsnag/js";
-
-        Bugsnag.start({apiKey: '91931cee1cbae5feec1925a566386158'})
-    </script>
 </svelte:head>
 
 <slot/>
@@ -18,5 +12,9 @@
     import '$lib/assets/fonts.css'
 
     inject({mode: (dev ? 'development' : 'production')});
+
+    import Bugsnag from "@bugsnag/js";
+
+    Bugsnag.start({apiKey: '91931cee1cbae5feec1925a566386158'})
 </script>
 
