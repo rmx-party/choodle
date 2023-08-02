01ecaad093b7cba1b1ba76802018d7a0e282af8d
Steven R. Baker <steven@stevenrbaker.com>
2023-08-02 20:05:03 +0200

    Add different sizes in the srcset for the wordmark.

--- frontend/src/components/Wordmark.svelte
+++ frontend/src/components/Wordmark.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
-    import wordmark from '$lib/assets/choodle-wordmark-1080.png';
+    import wordmark from '$lib/assets/choodle-wordmark.png';
+    import wordmark140 from '$lib/assets/choodle-wordmark-140.png';
+    import wordmark420 from '$lib/assets/choodle-wordmark-420.png';
+    import wordmark1080 from '$lib/assets/choodle-wordmark-1080.png';
+    import wordmark1920 from '$lib/assets/choodle-wordmark-1920.png';
 
     export let fontSize = 'inherit';
 </script>
 
-<img class="wordmark" src={wordmark} alt="Choodle" style:font-size={fontSize} />
+<img class="wordmark" src={wordmark} alt="Choodle" style:font-size={fontSize}
+     srcset={`${wordmark} 88w, ${wordmark140} 140w, ${wordmark420} 420w, ${wordmark1080} 1080w, ${wordmark1920} 1920w`}/>
 
 <style>
     .wordmark {
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -8,7 +8,7 @@
     import star1 from "$lib/assets/star-1.png";
     import star2 from "$lib/assets/star-2.png";
     import opengraphChoodle from '$lib/assets/OpenGraph-Choodle-1200x630-2x.jpg';
-	import { page } from '$app/stores';
+    import {page} from '$app/stores';
 
     export let data;
 
@@ -17,17 +17,17 @@
 
 
 <MetaData
-    title="How to Choodle"
-    themeColor="#FEF40A"
-    url={$page.url}
-    imageUrl={opengraphChoodle}
-    imageAlt="Cursive writing of the word “Choodle” with doodle stars and a smiley face on a yellow background"
-    width="1200"
-    height="630"
-    description="Draw something that lasts forever. Express yourself with lo-fi doodles that are yours to keep, sell, or share."/>
+        title="How to Choodle"
+        themeColor="#FEF40A"
+        url={$page.url}
+        imageUrl={opengraphChoodle}
+        imageAlt="Cursive writing of the word “Choodle” with doodle stars and a smiley face on a yellow background"
+        width="1200"
+        height="630"
+        description="Draw something that lasts forever. Express yourself with lo-fi doodles that are yours to keep, sell, or share."/>
 
 <main id="main">
-    <Wordmark fontSize="4rem"/>
+    <Wordmark fontSize="88px"/>
     <p><img width="22" height="17" src={star1} alt="a doodle of a star"/>FWB Fest 2023<img width="22" height="17"
                                                                                            src={star2}
                                                                                            alt="a doodle of a star"/>
