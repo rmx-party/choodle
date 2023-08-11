3fd47393b8a4981f0ac269575c2238418ff5d2ae
Steven R. Baker <steven@stevenrbaker.com>
2023-08-11 22:05:28 +0200

    Fetch will always give us an array.

--- frontend/src/routes/choodles/+page.ts
+++ frontend/src/routes/choodles/+page.ts
@@ -1,16 +1,9 @@
 import {readOnlyClient} from "$lib/CMSUtils";
-import fp from "lodash/fp";
 
 export async function load({params}) {
     const choodles = await readOnlyClient.fetch(`*[_type == "choodle"]`);
 
-    if (choodles) {
-        return {
-            choodles: choodles,
-        };
-    }
     return {
-        status: 500,
-        body: new Error("Internal Server Error")
+        choodles: choodles,
     };
 }
