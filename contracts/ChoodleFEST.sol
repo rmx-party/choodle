1aa4f5b6c06ee43ee9ad5d69700239bfb7849354
Steven R. Baker <steven@stevenrbaker.com>
2023-08-14 23:17:59 +0200

    The token name is now ChoodleFEST

--- contracts/ChoodleFEST.sol
+++ contracts/ChoodleFEST.sol
@@ -34,7 +34,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract MyToken is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable, Ownable {
+contract ChoodleFEST is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,10 +1,10 @@
 import hre, {ethers} from "hardhat";
 
 async function main() {
-    const myToken = await ethers.deployContract("MyToken")
-    await myToken.waitForDeployment()
+    const choodleFest = await ethers.deployContract("ChoodleFEST")
+    await choodleFest.waitForDeployment()
 
-    console.log("deployed to:", await myToken.target);
+    console.log("deployed to:", await choodleFest.target);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
