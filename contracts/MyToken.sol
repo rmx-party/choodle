7cfc3832ddcda9182f7bc2411e576285599c05cf
Steven R. Baker <steven@stevenrbaker.com>
2023-08-14 22:44:17 +0200

    Add the ascii art.

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,4 +1,30 @@
 // SPDX-License-Identifier: MIT
+/*
+ *                ...
+ *              .;lxOXXKkdl,.
+ *            ;kXWXOkkk0NWMNk;
+ *          ;kNKd:.    ..;lkXNk'                'dOx;                                                                ..            ..'.
+ *        ;kNKl.            .co'              .dXMMMK,                                                              ;K0,          ;OXK0l.
+ *      .oXKl.                               ,OWWXXMNc                                                             .OMN:         ;KWdcKWd.
+ *     ;0Nx.                                :XMNx;xMMk.                                                            ;XMO.        .kMO',KMx.
+ *   .lXNd.                                :KMXc .OMX:                                                             cNMd         ,KMd.,KMx.
+ *  .oNWd.                                ,0MXc  cNM0'                                                            .OMMd         ,KMd.oWX:
+ *  cNMO.                                .xWX:  .dWWl                                                             '0MMd         lNWo'kWx.
+ * .OMX;                                 :NWl   cNMO'                                           ...           .,'.oWMWo        .OM0;lW0'
+ * lNMx.                                .OMO.  '0MX:                      .''.          .;loccdO0000kooooooox0XWNKNMM0,        .OMOo0Nc
+ * .xMMx.                                lNXc  .kMWd.   ...             .lOXWWXOl;'..,lx0XWMMWKKWMWX00000XWMMMMWWMMMMMO.        .OMNXXo.
+ * .xMX:                                .OM0' ;OWXl.  .c0Xk'           :KNX0OKWMWWNXNWMMMMKoox:,OMWx....'xWW0l;;;:OWMMO.        .OMMWd.          .....
+ *  cXX:                                cNNo'lXWO;   'xNMM0'         .lXO;.. ,0MXko:::oKMX;     ;XMK,   lNWd.    '0MMMO.        ;KMWk.        .:xOOOOkd:.
+ *  .kMk.                              .OMN00NO:.   cKX0XWo          :Xk.    .kMOl.   ,0No      'OM0'  ,KMK,     oNMMMO.        dMWx.       .cO0d;'':xXO'
+ *   ;XNl                             .dWMMWO;    .dN0;lNWd.        ,KWo     .kMOl.   oMX;     .dWWo   ;XMd     '0WNNMX:       :XNo.      ;xKXd..,lxK0l.
+ *    oWNo.                          .oNMMXc     'OWK, '0MX:       ,0WWo     'OMOl.   dMNo...,ckNXo.   :NMd    'kWO:dWMx.    ,xNMN:   ..;kNMMN0k00kl;.
+ *    .dNW0c.                       .dNWWMO.   .lKWO,   cNWKc    .lXMMMk'   ,OWNl.    ,OWWXKKNWNO;     '0MO:;cxXW0' .OMNd;cdkXXk0W0ddx0K0OKMWkc:;.
+ *      cXMW0c.                   .cOXOcxWX;  ,kWNd.     :0WNx;,lKWNxxXWXkdkXWXo.      .,lxkkd:'        'kNMMMWKo.   'd0NNWXkc. .:clxko;. '0W0;            ..
+ *       .oONWKx:.            .;lkKNk;. ;KWklxNWK:        .ckOOkOxl,  .;okOOOd'                           ,lll;.       ..''.               .dXXx;.     .';x0o.
+ *          ,oOKXXOocccccccclkKWMNk;     ,x0K0xc.                                                                                            'lk00klcdkKX0x,
+ *             .';:lxxxxxxxxxxxo:,.        ...                                                                                                  .,:oxxo:,.
+ *
+ */
 pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
