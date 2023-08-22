7026c1ed9c18bc505dbff344aeafcda68bb80fe7
Steven R. Baker <steven@stevenrbaker.com>
2023-08-22 23:35:28 +0200

    Attach Mr. Robert Ross to the test email.

--- frontend/src/routes/api/testmail/+server.ts
+++ frontend/src/routes/api/testmail/+server.ts
@@ -1,11 +1,22 @@
 import sgMail from "@sendgrid/mail";
 import { SENDGRID_API_KEY } from "$env/static/private";
 import { json } from "@sveltejs/kit";
+import * as fs from "fs";
+
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 sgMail.setApiKey(SENDGRID_API_KEY)
 
 export const POST = async (req) => {
   console.log(`mail request`, req)
+
+  const pathToAttachment = `${__dirname}/../../../../static/choodle-bob-p2.png`;
+  const attachment = fs.readFileSync(pathToAttachment).toString("base64");
 
   const msg = {
     to: "dees@rmx.party",
@@ -13,6 +24,14 @@ export const POST = async (req) => {
     subject: "Sending with SendGrid is Fun",
     text: "and easy to do anywhere, even with Node.js",
     html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+    attachments: [
+      {
+        content: attachment,
+        filename: "bob.png",
+        type: "application/png",
+        disposition: "attachment"
+      }
+    ]
   };
 
   console.log("Form submitted");
