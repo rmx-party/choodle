558585c4c79eed5944a9b7de42c4510ec603ab31
Steven R. Baker <steven@stevenrbaker.com>
2023-08-25 12:27:36 -0700

    Fetch the choodle image and place it on the CoA.

--- frontend/src/lib/CertificateGenerator.test.ts
+++ frontend/src/lib/CertificateGenerator.test.ts
@@ -1,21 +1,25 @@
 import {describe, expect, it} from 'vitest';
 
 import Jimp from "jimp";
+import {readOnlyClient} from "$lib/CMSUtils";
+import {urlFor} from "$lib/PersistedImagesUtils";
+import * as fs from "fs";
 
 describe('load an image', () => {
     it('loads the correct file by checking its size', async () => {
-        const blankCertificatePath = "/Users/srbaker/Projects/rmx-party/choodle/frontend/src/lib/assets/Choodle-CoA-portrait-blank.png"
+        const blankCertificatePath = "/Users/srbaker/Projects/rmx-party/choodle/frontend/src/lib/assets/CoA-blank.png"
         const image = await Jimp.read(blankCertificatePath)
+        const choodleId = "nylCqCwRnhr26khFEUw8oj"
 
-        // FIXME: this needs to come from Sanity
-        const testChoodlePath = "/Users/srbaker/Projects/rmx-party/choodle/frontend/src/lib/assets/test-certificate-embeddable-image.png"
-        const choodle = await Jimp.read(testChoodlePath)
+        const choodles = await readOnlyClient.fetch(`*[_type == "choodle" && _id == "${choodleId}"]`)
+        const choodleImage = await Jimp.read(urlFor(choodles[0].image).toString())
+        choodleImage.scale(3)
 
-        image.blit(choodle, 655, 595)
+        image.blit(choodleImage, 665, 675)
         const font = await Jimp.loadFont(Jimp.FONT_SANS_64_BLACK)
 
-        const createdAt = "2023-07-21T18:15:30.000Z"
-        const creatorId = "17527cb1-90c3-453e-a269-d5c141735ce7"
+        const createdAt = choodles[0].createdAt
+        const creatorId = choodles[0].creatorId
 
         image.print(font, 400, 1700, `Made on: ${createdAt}`)
         image.print(font, 400, 1780, "Edition: 1/1")
