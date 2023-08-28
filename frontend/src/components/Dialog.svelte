2f079ff47fc66d31fb8a32e8e9f17434650ce89a
Brandon Dees <dees@pm.me>
2023-08-28 15:31:10 -0500

    adjust modal for safari mobile url bar

--- frontend/src/components/Dialog.svelte
+++ frontend/src/components/Dialog.svelte
@@ -72,6 +72,8 @@
     display: flex;
     flex-direction: column;
     flex-wrap: nowrap;
+
+    padding-bottom: env(safe-area-inset-bottom); /* adjust for safari mobile chrome overlay */
   }
   :global([data-svelte-dialog-content] .content) {
     display: block;
