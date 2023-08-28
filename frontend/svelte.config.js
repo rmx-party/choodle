e8aef50982deed644a3f4141dbb234a1d64784f7
Steven R. Baker <steven@stevenrbaker.com>
2023-08-28 15:08:42 -0700

    Use the vercel adapter for deployment.

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import {vitePreprocess} from '@sveltejs/kit/vite';
 import * as child_process from "child_process";
 
@@ -20,11 +20,13 @@ const config = {
         // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
         // If your environment is not supported or you settled on a specific environment, switch out the adapter.
         // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-        adapter: adapter(),
+        adapter: adapter({
+            runtime: 'edge'
+        }),
         version: {
             name: name
         }
-    }
+    },
 };
 
 export default config;
