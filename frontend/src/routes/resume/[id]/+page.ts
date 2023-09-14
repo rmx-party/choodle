599632a3adb8727564a9a2055317decd59e75884
Steven R. Baker <steven@stevenrbaker.com>
2023-09-14 15:59:02 +0200

    Resume a choodle.

---
+++ frontend/src/routes/resume/[id]/+page.ts
@@ -0,0 +1,17 @@
+import {readOnlyClient} from "$lib/CMSUtils";
+
+export const ssr = false;
+
+export async function load({params}) {
+    const choodle = await readOnlyClient.fetch(`*[_type == "choodle" && _id == "${params.id}"]`);
+
+    if (choodle) {
+        return {
+            choodle: choodle[0],
+        };
+    }
+    return {
+        status: 500,
+        body: new Error("Internal Server Error")
+    };
+}
