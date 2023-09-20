69aebca1ec0f63251237689d33c16cf5167203a5
Brandon Dees <dees@pm.me>
2023-09-20 18:41:40 -0500

    re-use css from the view page on resume page to make it look a lil nicer

--- frontend/src/routes/c/[id]/resume/+page.svelte
+++ frontend/src/routes/c/[id]/resume/+page.svelte
@@ -25,5 +25,63 @@
       width='390' height='520' alt=''/>
   </div>
 
-  <Button variant="big primary yellow" on:click={resume}>Resume This Choodle</Button>
+  <menu>
+    <Button variant="big primary" on:click={resume}>Resume This Choodle</Button>
+  </menu>
 </div>
+
+<style>
+    :root {
+        text-align: center;
+    }
+
+    .container {
+        /* gap: 2rem; */
+        padding: 1rem 1rem;
+        display: flex;
+        flex-direction: column;
+        flex-wrap: nowrap;
+        align-content: stretch;
+        align-items: stretch;
+        justify-content: space-between;
+        height: 100vh;
+        height: calc(var(--vh, 1vh) * 100); /* https://css-tricks.com/the-trick-to-viewport-units-on-mobile/ */
+        width: 100%;
+        padding: 1.5rem;
+    }
+
+    menu {
+        display: flex;
+        flex-direction: row;
+        margin: 1rem 0;
+        padding: 0;
+        gap: 1rem;
+    }
+
+    .choodle-container {
+        flex-grow: 1;
+        margin: 2rem auto;
+        display: flex;
+        align-items: center;
+        padding: 0;
+        flex-grow: 1;
+        flex-shrink: 1;
+        max-height: 50%;
+        max-width: 100%;
+        aspect-ratio: 3/4;
+    }
+    img.choodle {
+        flex-shrink: 1;
+        flex-grow: 1;
+        max-height: 100%;
+        max-width: 100%;
+        image-rendering: pixelated;
+
+        border-radius: 0.22175rem;
+        box-shadow: 1px 1px 17.74193572998047px 0.8870968222618103px rgba(0, 0, 0, 0.12);
+    }
+
+    .tagline {
+        margin-top: 0rem;
+    }
+</style>
