ef5fe57ddc94cd06f2fe806b11035d04c815a8e5
Brandon Dees <dees@pm.me>
2023-09-26 17:04:04 -0500

    quick pass at success page layout

--- frontend/src/routes/game/cwf/success/+page.svelte
+++ frontend/src/routes/game/cwf/success/+page.svelte
@@ -1,11 +1,41 @@
 <script lang="ts">
     import Button from "../../../../components/Button.svelte";
     import {goto} from "$app/navigation";
+    import { onMount } from "svelte";
+    import { browser } from "$app/environment";
+
+    export let data;
+    // TODO: load content section from CMS
+
+    onMount(async () => {
+        if (!browser) return;
+
+        // Explicitly reset bg color since it sticks after being set on next page and then navigating back
+        let root = document.documentElement;
+        root.style.setProperty('--page-background-color', 'rgba(20, 21, 24, 0.03)');
+    })
 </script>
 
-<div>
+<div class="container">
+  <div class="content">
     <h1>You got it!</h1>
     <p>Send a Choodle back to continue your streak.</p>
+  </div>
 
-    <Button on:click={() => { goto('/game/cwf/pick')}} colour="yellow" variant="primary">Continue</Button>
+  <Button on:click={() => { goto('/game/cwf/pick')}} colour="yellow" variant="primary">Continue</Button>
 </div>
+
+
+<style>
+  .container {
+    display: flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    padding: 1.5rem;
+    text-align: center;
+    gap: 1.5rem;
+  }
+</style>
