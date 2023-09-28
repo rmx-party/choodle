171bee913a904421352437c54e5b5c02dd8db7a2
Steven R. Baker <steven@stevenrbaker.com>
2023-09-28 20:36:06 +0200

    Pull in the copy on the CWF pick page.

---
+++ frontend/src/routes/game/cwf/+page.ts
@@ -0,0 +1,25 @@
+import {readOnlyClient} from "$lib/CMSUtils";
+
+export async function load({params}) {
+    const copy = await readOnlyClient.fetch(`*[_type == "choodleWithFriendsCopy"] | order(_createdAt) [0]`)
+    const dummyGames = [ // TODO: speculative data model for page mockup purposes
+        {
+            _id: 1,
+            turn: 'some-player-id',
+            choodles: [], // Choodle association array
+            guesses: [] // Guess entries / counts per choodle perhaps? guesses belong to a particular choodle, and a game, and player
+        },
+        {
+            _id: 2,
+            turn: 'other-player-id',
+            choodles: [],
+            guesses: []
+        }
+    ]
+
+    console.log(`load data: `, copy)
+    return {
+        copy: copy,
+        liveGames: [...dummyGames],
+    }
+}
