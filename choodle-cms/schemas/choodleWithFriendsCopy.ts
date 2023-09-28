6dc2afbe6bf3b254cb06e44b5642a93dd1bc6897
Steven R. Baker <steven@stevenrbaker.com>
2023-09-28 20:14:38 +0200

    Add CWF copy to CMS.

---
+++ choodle-cms/schemas/choodleWithFriendsCopy.ts
@@ -0,0 +1,85 @@
+export default {
+    name: 'choodleWithFriends',
+    type: 'document',
+    title: 'Choodle With Friends',
+    fields: [
+        {
+            name: 'startGameButtonText',
+            type: 'string',
+            title: 'Start Game Button Text',
+        },
+        {
+            name: 'pick_promptSelectionPageTopContent',
+            title: 'Prompt Selection Page Top Content',
+            type: 'array',
+            of: [{type: 'block'}]
+        },
+        {
+            name: 'pick_shuffleButtonText',
+            type: 'string',
+            title: 'Shuffle Button Text',
+        },
+        {
+            name: 'pick_doneButtonText',
+            type: 'string',
+            title: 'Done Button Text',
+        },
+        {
+            name: 'draw_topBarContent',
+            title: 'Draw Page Top Bar Instruction',
+            type: 'string',
+        },
+        {
+            name: 'draw_undoButtonText',
+            title: 'Draw Page Undo Button Text',
+            type: 'string',
+        },
+        {
+            name: 'draw_doneButtonText',
+            title: 'Draw Page Done Button Text',
+            type: 'string',
+        },
+        {
+            name: 'share_messageText',
+            title: 'Share Page Message Text',
+            type: 'string',
+        },
+        {
+            name: 'guess_pageTopContent',
+            title: 'Guess Page Top Content',
+            type: 'array',
+            of: [{type: 'block'}]
+        },
+        {
+            name: 'guess_doneButtonText',
+            title: 'Guess Page Done Button Text',
+            type: 'string',
+        },
+        {
+            name: 'guess_failureMessageText',
+            title: 'Guess Page Failure Message Text',
+            type: 'string',
+        },
+        {
+            name: 'guess_failureNewGameButtonText',
+            title: 'Guess Page New Game Button Text',
+            type: 'string',
+        },
+        {
+            name: 'success_topContent',
+            title: 'Success Page New Game Button Text',
+            type: 'array',
+            of: [{type: 'block'}]
+        },
+        {
+            name: 'success_continueGameButtonText',
+            title: 'Success Page Continue Button Text',
+            type: 'string',
+        },
+        {
+            name: 'success_leaderboardButtonText',
+            title: 'Success Page Leaderboard Button Text',
+            type: 'string',
+        },
+    ]
+}
