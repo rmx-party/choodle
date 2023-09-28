82b221890c8924c32e1f405a800b1639877527a7
Steven R. Baker <steven@stevenrbaker.com>
2023-09-28 22:40:38 +0200

    Add CWF game to CMS.

---
+++ choodle-cms/schemas/choodleWithFriendsGame.ts
@@ -0,0 +1,17 @@
+export default {
+  name: 'choodleWithFriendsGame',
+  type: 'document',
+  title: 'Choodle With Friends Game',
+  fields: [
+    {
+      name: 'player1CreatorId',
+      type: 'string',
+      title: 'Player Who Started the Game',
+    },
+    {
+      name: 'gamePrompt',
+      type: 'string',
+      title: 'Game Prompt',
+    },
+  ]
+}
