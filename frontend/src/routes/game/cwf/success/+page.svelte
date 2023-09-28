bd3b645e100f81b404f76b2d51e938f044c855a1
Steven R. Baker <steven@stevenrbaker.com>
2023-09-28 21:05:34 +0200

    Use CMS copy in success page.

--- frontend/src/routes/game/cwf/success/+page.svelte
+++ frontend/src/routes/game/cwf/success/+page.svelte
@@ -1,29 +1,30 @@
 <script lang="ts">
-    import Button from "../../../../components/Button.svelte";
-    import {goto} from "$app/navigation";
-    import { onMount } from "svelte";
-    import { browser } from "$app/environment";
+  import Button from "../../../../components/Button.svelte";
+  import {goto} from "$app/navigation";
+  import {onMount} from "svelte";
+  import {browser} from "$app/environment";
+  import {toHTML} from "@portabletext/to-html";
 
-    export let data;
-    // TODO: load content sections from CMS
-    // TODO: load the image from CMS once the data model/routing supports it
+  export let data;
+  // TODO: load content sections from CMS
+  // TODO: load the image from CMS once the data model/routing supports it
 
-    onMount(async () => {
-        if (!browser) return;
+  onMount(async () => {
+    if (!browser) return;
 
-        // Explicitly reset bg color since it sticks after being set on next page and then navigating back
-        let root = document.documentElement;
-        root.style.setProperty('--page-background-color', 'rgba(20, 21, 24, 0.03)');
-    })
+    // Explicitly reset bg color since it sticks after being set on next page and then navigating back
+    let root = document.documentElement;
+    root.style.setProperty('--page-background-color', 'rgba(20, 21, 24, 0.03)');
+  })
 </script>
 
 <div class="container">
   <div class="content">
-    <h1>You got it!</h1>
-    <p>Send a Choodle back to continue your streak.</p>
+    {@html toHTML(data.copy.success_topContent)}
   </div>
 
-  <Button on:click={() => { goto('/game/cwf/pick')}} colour="yellow" variant="primary">Continue</Button>
+  <Button on:click={() => { goto('/game/cwf/pick')}} colour="yellow"
+          variant="primary">{data.copy.success_continueGameButtonText}</Button>
 </div>
 
 
