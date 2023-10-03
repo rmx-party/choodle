39729384ee07a4057b8260f61999b42444b458e7
Steven R. Baker <steven@stevenrbaker.com>
2023-10-03 19:50:14 +0200

    Start updating the guess input.

--- frontend/src/components/GuessInput.svelte
+++ frontend/src/components/GuessInput.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
   export let display;
+  export let cursorLocation;
 </script>
 
+<p>{cursorLocation}</p>
 <p>{display}</p>
 
 <style>
--- frontend/src/routes/game/keyboard/+page.svelte
+++ frontend/src/routes/game/keyboard/+page.svelte
@@ -3,13 +3,44 @@
   import GuessInput from "../../../components/GuessInput.svelte";
   import {writable} from "svelte/store";
 
-  let inputDisplay = writable('');
-  let format = "FOO BAR";
+  let inputDisplay = writable([])
+  let cursorLocation = writable(0)
+  let format = "FOOBAR".split('')
 
-  const handleKeyPress = (keyPress: string) => {
-    inputDisplay.set('hello')
+  const moveCursorRight = () => {
+    if ($cursorLocation === format.length - 1) return;
+
+    cursorLocation.update(currentCursorLocation => currentCursorLocation + 1)
+  }
+
+  const moveCursorLeft = () => {
+    if ($cursorLocation < 1) {
+      cursorLocation.set(0)
+      return;
+    }
+    cursorLocation.update(currentCursorLocation => currentCursorLocation - 1)
+  }
+
+  const updateAtCursor = (key) => {
+    inputDisplay.update(currentInputDisplay => {
+      currentInputDisplay[$cursorLocation] = key
+      return currentInputDisplay
+    })
+  }
+
+  const handleKeyPress = (key: string) => {
+    if (key !== "BACKSPACE" && key !== "ENTER") {
+      updateAtCursor(key)
+      moveCursorRight()
+    }
+    // handle spaces
+
+    if (key === "BACKSPACE") {
+      updateAtCursor('')
+      moveCursorLeft()
+    }
   }
 </script>
 
 <Keyboard onKeyPress={handleKeyPress}/>
-<GuessInput format={format} display={$inputDisplay}/>
+<GuessInput format={format} display={$inputDisplay.join('')} cursorLocation={$cursorLocation}/>
