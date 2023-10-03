49becff05153db41424b290247510a4014b6914c
Steven R. Baker <steven@stevenrbaker.com>
2023-10-03 19:31:48 +0200

    Add a GuessInput component.

---
+++ frontend/src/components/GuessInput.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  export let display;
+</script>
+
+<p>{display}</p>
+
+<style>
+
+</style>
--- frontend/src/routes/game/keyboard/+page.svelte
+++ frontend/src/routes/game/keyboard/+page.svelte
@@ -1,5 +1,15 @@
 <script lang="ts">
   import Keyboard from "../../../components/Keyboard.svelte";
+  import GuessInput from "../../../components/GuessInput.svelte";
+  import {writable} from "svelte/store";
+
+  let inputDisplay = writable('');
+  let format = "FOO BAR";
+
+  const handleKeyPress = (keyPress: string) => {
+    inputDisplay.set('hello')
+  }
 </script>
 
-<Keyboard/>
+<Keyboard onKeyPress={handleKeyPress}/>
+<GuessInput format={format} display={$inputDisplay}/>
