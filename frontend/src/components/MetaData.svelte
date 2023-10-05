3a2e08d58f55ede382b49539b6f1f09e89c4d286
Brandon Dees <dees@pm.me>
2023-10-04 22:54:35 -0500

    extend configuration values, refine metadata

--- frontend/src/components/MetaData.svelte
+++ frontend/src/components/MetaData.svelte
@@ -1,46 +1,49 @@
 <script lang="ts">
   import { browser } from "$app/environment";
+  import { backgroundColour } from "$lib/Configuration";
+  import { setDynamicBackground } from "$lib/dynamicBackground";
 
-    export let url: string | undefined = undefined;
-    export let title: string | undefined = undefined;
-    export let imageUrl: string | undefined = undefined;
-    export let imageAlt: string | undefined = undefined;
-    export let width: number | string | undefined = undefined;
-    export let height: number | string | undefined = undefined;
-    export let description: string | undefined = undefined;
-    export let themeColor: string | null = '#FEF40A';
+  export let url: URL | string | undefined = undefined;
+  export let title: string | undefined = undefined;
+  export let imageUrl: string | undefined = undefined;
+  export let imageAlt: string | undefined = undefined;
+  export let width: number | string | undefined = undefined;
+  export let height: number | string | undefined = undefined;
+  export let description: string | undefined = undefined;
+  export let themeColor: string | null = backgroundColour;
 
-    if (browser) {
-      console.log('setting theme color', themeColor)
-      document?.querySelector('meta[name=theme-color]')?.setAttribute('content', `${themeColor}`)
-    }
+  if (browser && themeColor) {
+    console.log('setting theme color', themeColor)
+    document?.querySelector('meta[name=theme-color]')?.setAttribute('content', `${themeColor}`)
+    setDynamicBackground(themeColor);
+  }
 </script>
 
 <svelte:head>
-    <!-- TODO: set canonical URL (decide trailing slash etc) -->
-    <meta property="og:type" content='website'/>
-    {#if url}
-        <meta property="og:url" content={url}/>
+  <!-- TODO: set canonical URL (decide trailing slash etc) -->
+  <meta property="og:type" content='website'/>
+  {#if url}
+    <meta property="og:url" content={`${url}`}/>
+  {/if}
+  <meta property="og:site_name" content="Choodle"/>
+  {#if title}
+    <title>{title}</title>
+    <meta property="og:title" content={title}/>
+  {/if}
+  {#if imageUrl}
+    <meta property="og:image" content={imageUrl}/>
+    <meta property="og:image:secure_url" content={imageUrl}/>
+    <meta property="og:image:width" content={`${width}`}/>
+    <meta property="og:image:height" content={`${height}`}/>
+    {#if imageAlt}
+      <meta property="og:image:alt" content={imageAlt}/>
     {/if}
-    <meta property="og:site_name" content="Choodle"/>
-    {#if title}
-        <title>{title}</title>
-        <meta property="og:title" content={title}/>
-    {/if}
-    {#if imageUrl}
-        <meta property="og:image" content={imageUrl}/>
-        <meta property="og:image:secure_url" content={imageUrl}/>
-        <meta property="og:image:width" content={width}/>
-        <meta property="og:image:height" content={height}/>
-        {#if imageAlt}
-            <meta property="og:image:alt" content={imageAlt}/>
-        {/if}
-    {/if}
-    {#if description}
-        <meta property="og:description" content={description}/>
-        <meta name="description" content={description}/>
-    {/if}
-    {#if themeColor}
-        <meta name="theme-color" content={themeColor}/>
-    {/if}
+  {/if}
+  {#if description}
+    <meta property="og:description" content={description}/>
+    <meta name="description" content={description}/>
+  {/if}
+  {#if themeColor}
+    <meta name="theme-color" content={themeColor}/>
+  {/if}
 </svelte:head>
--- frontend/src/lib/Configuration.ts
+++ frontend/src/lib/Configuration.ts
@@ -3,7 +3,9 @@ import type {Dimensiony} from "$lib/Calculations";
 export const lineWidth = 1;
 export const targetMaxSize: Dimensiony = {x: 195, y: 260}
 export const drawColor = "#000000"
-export const backgroundColour = "#ffffff"
+export const backgroundColour = "#FFFFFF"
+export const pageBackgroundDefault = "#F7F7F7"
+export const choodleYellow = "#FEF40A"
 export const choodleUndoKey = 'choodle-undo'
 export const choodlePromptKey = 'choodle-selected-prompt'
 export const choodleCreatorEmailKey = 'choodle-creator-email'
