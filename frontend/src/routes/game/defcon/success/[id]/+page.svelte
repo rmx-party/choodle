cd9fb2a568c40e87f3bc81d6ff3dcbc454764967
Brandon Dees <dees@pm.me>
2023-10-05 17:00:11 -0500

    update layout and metadata for defcon success page

--- frontend/src/routes/game/defcon/success/[id]/+page.svelte
+++ frontend/src/routes/game/defcon/success/[id]/+page.svelte
@@ -1,23 +1,23 @@
 <script lang="ts">
   import Button from "../../../../../components/Button.svelte";
   import {goto} from "$app/navigation";
-  import {onMount} from "svelte";
-  import {browser} from "$app/environment";
   import {toHTML} from "@portabletext/to-html";
   import {urlFor} from "$lib/PersistedImagesUtils";
+  import LayoutContainer from "../../../../../components/LayoutContainer.svelte";
+  import MetaData from "../../../../../components/MetaData.svelte";
+  import { page } from "$app/stores";
+  import {pageBackgroundDefault} from '$lib/Configuration';
 
   export let data;
-
-  onMount(async () => {
-    if (!browser) return;
-
-    // Explicitly reset bg color since it sticks after being set on next page and then navigating back
-    let root = document.documentElement;
-    root.style.setProperty('--page-background-color', 'rgba(20, 21, 24, 0.03)');
-  })
 </script>
 
-<div class="container">
+<MetaData
+  title="Choodle w/ Friends: DEFcon Edition"
+  themeColor={pageBackgroundDefault}
+  url={$page.url}
+/>
+
+<LayoutContainer --layout-justify='space-evenly'>
   <div class="content">
     {@html toHTML(data.copy.success_topContent)}
   </div>
@@ -33,19 +33,5 @@
     <Button on:click={() => { goto('/game/defcon/pick')}}
             colour="yellow">{data.copy.success_continueGameButtonText}</Button>
   </div>
-</div>
+</LayoutContainer>
 
-
-<style>
-  .container {
-    display: flex;
-    flex-direction: column;
-    flex-wrap: nowrap;
-    align-items: center;
-    justify-content: center;
-    height: 100vh;
-    padding: 1.5rem;
-    text-align: center;
-    gap: 1.5rem;
-  }
-</style>
