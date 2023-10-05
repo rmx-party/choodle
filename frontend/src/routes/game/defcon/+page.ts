cec6f2c422f24d769b6ade3c7c2e28840bb52783
Steven R. Baker <steven@stevenrbaker.com>
2023-10-05 23:20:42 +0200

    We don't need the dummy games.

--- frontend/src/routes/game/defcon/+page.ts
+++ frontend/src/routes/game/defcon/+page.ts
@@ -4,26 +4,9 @@ export async function load({params}) {
   const copy = await readOnlyClient.fetch(`*[_type == "choodleWithFriendsCopy" && gameName  == "defcon"] | order(_createdAt) [0]`)
   const challenges = await readOnlyClient.fetch(`*[_type == "challenge" && game == "defcon"]{..., choodle, challenger->{username}} | order(_createdAt desc)`)
 
-  const
-    dummyGames = [ // TODO: speculative data model for page mockup purposes
-      {
-        _id: 1,
-        turn: 'some-player-id',
-        choodles: [], // Choodle association array
-        guesses: [] // Guess entries / counts per choodle perhaps? guesses belong to a particular choodle, and a game, and player
-      },
-      {
-        _id: 2,
-        turn: 'other-player-id',
-        choodles: [],
-        guesses: []
-      }
-    ]
-
   console.log(`load data: `, copy, challenges)
   return {
     copy,
     challenges,
-    liveGames: [...dummyGames],
   }
 }
