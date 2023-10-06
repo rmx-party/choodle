080e06e7dc21662ce67e42f27e8fc2a947501250
Brandon Dees <dees@pm.me>
2023-10-05 20:00:18 -0500

    cleanup

--- frontend/src/routes/game/cwf/success/[id]/+page.svelte
+++ frontend/src/routes/game/cwf/success/[id]/+page.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
   import Button from "../../../../../components/Button.svelte";
   import {goto} from "$app/navigation";
-  import {onMount} from "svelte";
-  import {browser} from "$app/environment";
   import {toHTML} from "@portabletext/to-html";
   import {urlFor} from "$lib/PersistedImagesUtils";
   import MetaData from "../../../../../components/MetaData.svelte";
@@ -11,19 +9,11 @@
   import LayoutContainer from "../../../../../components/LayoutContainer.svelte";
 
   export let data;
-
-  onMount(async () => {
-    if (!browser) return;
-
-    // Explicitly reset bg color since it sticks after being set on next page and then navigating back
-    let root = document.documentElement;
-    root.style.setProperty('--page-background-color', 'rgba(20, 21, 24, 0.03)');
-  })
 </script>
 
 
 <MetaData
-  title="Choodle w/ Friends: DEFcon Edition"
+  title="Choodle with Friends"
   themeColor={choodleYellow}
   bgColor={pageBackgroundDefault}
   url={$page.url}
@@ -39,7 +29,7 @@
       width='390' height='520' alt=''/>
   </div>
 
-  <h3><strong>{data.choodle.gamePrompt.toUpperCase()}</strong></h3>
+  <h3><strong>{data.choodle?.gamePrompt?.toUpperCase() || ''}</strong></h3>
 
   <div>
     <Button on:click={() => { goto('/game/cwf/pick')}}
