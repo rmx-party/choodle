bc5b6b0e267e469ad6deeb6ab40aa758230126c8
Steven R. Baker <steven@stevenrbaker.com>
2023-10-16 22:40:16 +0200

    The hints aren't required.

--- choodle-cms/schemas/gamePrompt.ts
+++ choodle-cms/schemas/gamePrompt.ts
@@ -14,28 +14,28 @@ export default {
       name: 'hint',
       type: 'string',
       title: 'Hint 1',
-      validation: Rule => Rule.required().max(60),
+      validation: Rule => Rule.max(60),
       description: 'Max 60 characters.'
     },
     {
       name: 'hint_2',
       type: 'string',
       title: 'Hint 2',
-      validation: Rule => Rule.required().max(60),
+      validation: Rule => Rule.max(60),
       description: 'Max 60 characters.'
     },
     {
       name: 'hint_3',
       type: 'string',
       title: 'Hint 3',
-      validation: Rule => Rule.required().max(60),
+      validation: Rule => Rule.max(60),
       description: 'Max 60 characters.'
     },
     {
       name: 'promptType',
       type: 'string',
       title: 'Prompt Type',
-      validation: Rule => Rule.required().max(60),
+      validation: Rule => Rule.max(60),
       description: 'Max 60 characters.',
       initialValue: 'choodle-with-friends-word',
     },
