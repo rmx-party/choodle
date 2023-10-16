f66b1f37270c220600737969a059e89a1715794f
Steven R. Baker <steven@stevenrbaker.com>
2023-10-16 22:02:13 +0200

    Add two more hint fields.

--- choodle-cms/schemas/gamePrompt.ts
+++ choodle-cms/schemas/gamePrompt.ts
@@ -13,7 +13,21 @@ export default {
     {
       name: 'hint',
       type: 'string',
-      title: 'Hint',
+      title: 'Hint 1',
+      validation: Rule => Rule.required().max(60),
+      description: 'Max 60 characters.'
+    },
+    {
+      name: 'hint_2',
+      type: 'string',
+      title: 'Hint 2',
+      validation: Rule => Rule.required().max(60),
+      description: 'Max 60 characters.'
+    },
+    {
+      name: 'hint_3',
+      type: 'string',
+      title: 'Hint 3',
       validation: Rule => Rule.required().max(60),
       description: 'Max 60 characters.'
     },
