78e2462f2274b45161893965e04f336a89e67725
Brandon Dees <dees@pm.me>
2023-10-17 16:56:44 -0500

    copy and other page resources can load concurrently

--- frontend/src/routes/game/cwf/guess/[id]/+page.ts
+++ frontend/src/routes/game/cwf/guess/[id]/+page.ts
@@ -1,10 +1,12 @@
 import {readOnlyClient} from "$lib/CMSUtils";
 
 export async function load({params}) {
-  const copy = await readOnlyClient.fetch(`*[_type == "choodleWithFriendsCopy"] | order(_createdAt) [0]`)
-  const challenge = await readOnlyClient.fetch(`*[_type == "challenge" && _id == "${params.id}"]{..., choodle->{...}, gamePromptRef->{...}} [0]`);
+  const copyPromise = await readOnlyClient.fetch(`*[_type == "choodleWithFriendsCopy"] | order(_createdAt) [0]`)
+  const challengePromise = await readOnlyClient.fetch(`*[_type == "challenge" && _id == "${params.id}"]{..., choodle->{...}, gamePromptRef->{...}} [0]`);
 
-  if (challenge) return {copy, challenge: challenge, gamePrompt: challenge.gamePromptRef, choodle: challenge.choodle};
+  const [copy, challenge] = await Promise.all([copyPromise, challengePromise])
+
+  if (challenge && copy) return {copy, challenge, gamePrompt: challenge.gamePromptRef, choodle: challenge.choodle};
 
   return {
     status: 500,
