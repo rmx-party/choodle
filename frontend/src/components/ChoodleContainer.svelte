af4d8ccb492a4d39c96d24d366db92116bf3930c
Brandon Dees <dees@pm.me>
2023-10-18 16:29:28 -0500

    DRY tidy choodle container CSS

--- frontend/src/components/ChoodleContainer.svelte
+++ frontend/src/components/ChoodleContainer.svelte
@@ -3,25 +3,25 @@
 </div>
 
 <style>
-  .choodle-container {
+  .choodle-container,
+  .choodle-container > :global(*) {
     flex-grow: 1;
     aspect-ratio: 3/4;
+
+    max-height: clamp(100px, 100%, calc(100svh - var(--choodle-max-height-offset, 60svh)));
+    max-width: min(100%, 100svw);
+  }
+
+  .choodle-container {
     display: flex;
     align-items: center;
     justify-content: center;
     padding: 0;
     margin: 1rem;
-    max-height: clamp(100px, 100%, calc(100svh - var(--choodle-max-height-offset, 60svh)));
-    max-width: min(100%, 100svw);
   }
 
   .choodle-container > :global(*) {
-    flex-grow: 1;
-    aspect-ratio: 3/4;
-    max-height: clamp(100px, 100%, calc(100svh - var(--choodle-max-height-offset, 60svh)));
-    max-width: min(100%, 100svw);
     image-rendering: pixelated;
-
     border-radius: 0.22175rem;
     box-shadow: 1px 1px 17.74193572998047px 0.8870968222618103px rgba(0, 0, 0, 0.12);
   }
