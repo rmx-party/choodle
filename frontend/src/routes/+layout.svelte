6190418fce30e47cf061cbb416fc6f7de00265af
Brandon Dees <dees@pm.me>
2023-10-18 19:53:05 -0500

    use the loading indicator globally from the main layout since it handles its own visibility

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,10 +1,8 @@
-
-<slot/>
-
 <script lang="ts">
     import {browser, dev} from "$app/environment"
     import '$lib/assets/fonts.css'
     import '../app.css'
+    import LoadingIndicator from "../components/LoadingIndicator.svelte";
 
     import Bugsnag from "@bugsnag/js";
 
@@ -12,3 +10,6 @@
       Bugsnag.start({apiKey: '91931cee1cbae5feec1925a566386158'})
     }
 </script>
+
+<LoadingIndicator />
+<slot/>
