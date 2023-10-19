5da4c741ee1775b8987b774dfa6ea2708aa826a7
Brandon Dees <dees@pm.me>
2023-10-18 20:00:51 -0500

    tidy the old draw page to lean on the global loading indicator

--- frontend/src/routes/draw/+page.svelte
+++ frontend/src/routes/draw/+page.svelte
@@ -1,30 +1,26 @@
 <script lang="ts">
-    import {loading, loadingMessage} from "$lib/store";
-    import LoadingIndicator from "../../components/LoadingIndicator.svelte";
-    import MetaData from "../../components/MetaData.svelte";
-    import {page} from "$app/stores";
-    import opengraphChoodle from '$lib/assets/OpenGraph-Choodle-1200x630-2x.jpg';
-    import CertifiableChoodleBoard from "../../components/CertifiableChoodleBoard.svelte";
+  import MetaData from "../../components/MetaData.svelte";
+  import {page} from "$app/stores";
+  import opengraphChoodle from '$lib/assets/OpenGraph-Choodle-1200x630-2x.jpg';
+  import CertifiableChoodleBoard from "../../components/CertifiableChoodleBoard.svelte";
+  import { choodleYellow } from "$lib/Configuration";
 
-    export let data;
+  export let data;
 </script>
 
 <MetaData
-    url={$page.url}
-    title="Draw a Choodle"
-    imageUrl={opengraphChoodle}
-    imageAlt="Cursive writing of the word “Choodle” with doodle stars and a smiley face on a yellow background"
-    width="1200"
-    height="630"
-    description="Draw something that lasts forever. Express yourself with lo-fi doodles that are yours to keep, sell, or share."/>
+  url={$page.url}
+  title="Draw a Choodle"
+  themeColor={choodleYellow}
+  imageUrl={opengraphChoodle}
+  imageAlt="Cursive writing of the word “Choodle” with doodle stars and a smiley face on a yellow background"
+  width="1200"
+  height="630"
+  description="Draw something that lasts forever. Express yourself with lo-fi doodles that are yours to keep, sell, or share."/>
 
-{#if !$loading}
-    <div id="choodle-box">
-        <CertifiableChoodleBoard id="choodle-board" prompt={data.prompt} certificateModal={data.certificateModal}></CertifiableChoodleBoard>
-    </div>
-{:else}
-    <LoadingIndicator explanation={$loadingMessage}></LoadingIndicator>
-{/if}
+<div id="choodle-box">
+  <CertifiableChoodleBoard id="choodle-board" prompt={data.prompt} certificateModal={data.certificateModal}></CertifiableChoodleBoard>
+</div>
 
 <style>
 #choodle-box {
