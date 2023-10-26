bb181fdf83c8846d2d6aa63131027e3f57f679c1
Steven R. Baker <steven@stevenrbaker.com>
2023-10-26 23:54:39 +0200

    Revert "Put AppSignal stuff in the server hook."
    This reverts commit 4a11897cb033042b201f3d79d854751a15709ed3.

--- frontend/src/hooks.server.ts
+++
@@ -1,8 +0,0 @@
-import {Appsignal} from "@appsignal/nodejs";
-
-console.log(`Appsignal key: ${process.env.APPSIGNAL_PUSH_API_KEY}`)
-new Appsignal({
-  active: true,
-  name: "Choodle",
-  pushApiKey: `${process.env.APPSIGNAL_PUSH_API_KEY}`
-});
