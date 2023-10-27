4bdc30d43528fd114a632f8740fb7edfbc002099
Brandon Dees <dees@pm.me>
2023-10-27 17:48:23 -0500

    try testing vercel page speed insights

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,12 +1,24 @@
 <script lang="ts">
     import {browser, dev} from "$app/environment"
     import {isOnline} from "$lib/store"
+    import {webVitals} from "$lib/vitals"
     import '$lib/assets/fonts.css'
     import { onMount } from "svelte";
     import '../app.css'
     import LoadingIndicator from "../components/LoadingIndicator.svelte";
-
     import Bugsnag from "@bugsnag/js";
+    import { page } from "$app/stores";
+
+    let analyticsId = import.meta.env.VERCEL_ANALYTICS_ID;
+
+    $: if (browser && analyticsId) {
+      webVitals({
+        path: $page.url.pathname,
+        params: $page.params,
+        analyticsId
+      })
+    }
+
 
     if (browser && !dev) {
       Bugsnag.start({apiKey: '91931cee1cbae5feec1925a566386158'})
