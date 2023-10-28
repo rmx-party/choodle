7872bc019b4941fb8bb700c5aa3728cb6b5150e2
Brandon Dees <dees@pm.me>
2023-10-27 21:17:04 -0500

    refactor cms client constructors to enable a cdn variant cleanly

--- frontend/src/lib/CMSUtils.ts
+++ frontend/src/lib/CMSUtils.ts
@@ -1,20 +1,25 @@
 import {createClient} from "@sanity/client";
 import {PUBLIC_SANITY_DATASET, PUBLIC_SANITY_PROJECT_ID, PUBLIC_SANITY_TOKEN} from "$env/static/public";
 
-export const readWriteClient = createClient({
+const baseConfig = {
   projectId: PUBLIC_SANITY_PROJECT_ID,
   dataset: PUBLIC_SANITY_DATASET,
   token: PUBLIC_SANITY_TOKEN,
   apiVersion: "2023-07-18",
+}
+export const readWriteClient = createClient({
+  ...baseConfig,
   useCdn: false,
 })
 
 export const readOnlyClient = createClient({
-  projectId: PUBLIC_SANITY_PROJECT_ID,
-  dataset: PUBLIC_SANITY_DATASET,
-  perspective: 'published',
-  apiVersion: "2023-07-18",
+  ...baseConfig,
   useCdn: false,
+})
+
+export const cachedReadOnlyClient = createClient({
+  ...baseConfig,
+  useCdn: true,
 })
 
 export const getChoodleById = async (choodleId) => {
