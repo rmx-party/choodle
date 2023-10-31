9d30ddd95f01e09884bad9e4fea21e73c4a5b06c
Brandon Dees <dees@pm.me>
2023-10-31 16:55:53 -0500

    tidy

--- frontend/src/routes/game/cwf/+page.ts
+++ frontend/src/routes/game/cwf/+page.ts
@@ -1,9 +1,6 @@
-import {cachedReadOnlyClient, readOnlyClient} from "$lib/CMSUtils";
-import {loading} from "$lib/store";
+import { cachedReadOnlyClient, readOnlyClient } from "$lib/CMSUtils";
 
-export async function load({params}) {
-  loading.set(true)
-
+export async function load() {
   return {
     copy: cachedReadOnlyClient.fetch(`*[_type == "choodleWithFriendsCopy"] | order(_createdAt) [0]`),
     games: readOnlyClient.fetch(`*[_type == "cwfgame"]{..., currentChallenge->{..., challenger->{...}}, player1->{...}, player2->{...}, guessResults[]->{..., challenge->{...}}} | order(_createdAt desc)`),
