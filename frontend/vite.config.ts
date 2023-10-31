363c10d06e8cf7486cd11d417cc37fb72fd45c68
Brandon Dees <dees@pm.me>
2023-10-30 21:41:02 -0500

    so the problem is at the vite build stage, let's see if it's with source maps or auto instrument

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,26 +4,31 @@ import {defineConfig} from 'vitest/config';
 import * as path from 'path';
 
 export default defineConfig({
-    plugins: [sveltekit()],
-    // plugins: [sentrySvelteKit({
-    //   sourceMapsUploadOptions: {
-    //     org: "rmx-party",
-    //     project: "javascript-sveltekit",
-    //   }
-    // }), sveltekit()],
-    build: {
-      sourcemap: true,
+  // plugins: [sveltekit()],
+  plugins: [sentrySvelteKit({
+    adapter: "vercel",
+    sourceMapsUploadOptions: {
+      org: "rmx-party",
+      project: "javascript-sveltekit",
     },
-    test: {
-      include: ['src/**/*.{test,spec}.{js,ts}'],
-      reporters: ['default', 'html']
-    },
-    define: {
-      'process.env': process.env
-    },
-    resolve: {
-      alias: {
-        '~': path.resolve(__dirname, 'src')
-      }
+    autoInstrument: {
+      load: false,
+      serverLoad: false,
     }
-  });
+  }), sveltekit()],
+  build: {
+    sourcemap: true,
+  },
+  test: {
+    include: ['src/**/*.{test,spec}.{js,ts}'],
+    reporters: ['default', 'html']
+  },
+  define: {
+    'process.env': process.env
+  },
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, 'src')
+    }
+  }
+});
