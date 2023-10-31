6e4929c7db1d4d12c2010c7dc1a9b7bbc0f76f3a
Brandon Dees <dees@pm.me>
2023-10-30 20:28:52 -0500

    troubleshooting sentry config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,26 +3,28 @@ import {sveltekit} from '@sveltejs/kit/vite';
 import {defineConfig} from 'vitest/config';
 import * as path from 'path';
 
+console.log(`sentry token: ${process.env.SENTRY_AUTH_TOKEN}`);
 export default defineConfig({
     plugins: [sentrySvelteKit({
-        sourceMapsUploadOptions: {
-            org: "rmx-party",
-            project: "javascript-sveltekit"
-        }
+      sourceMapsUploadOptions: {
+        org: "rmx-party",
+        project: "javascript-sveltekit",
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+      }
     }), sveltekit()],
     build: {
-        sourcemap: true,
+      sourcemap: true,
     },
     test: {
-        include: ['src/**/*.{test,spec}.{js,ts}'],
-        reporters: ['default', 'html']
+      include: ['src/**/*.{test,spec}.{js,ts}'],
+      reporters: ['default', 'html']
     },
     define: {
-        'process.env': process.env
+      'process.env': process.env
     },
-  resolve: {
-    alias: {
-      '~': path.resolve(__dirname, 'src')
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname, 'src')
+      }
     }
-  }
-});
+  });
