7a62afd0374c051489f9a0e74f1338b75c09c515
Brandon Dees <dees@pm.me>
2023-10-30 21:11:12 -0500

    test if it's server only

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -3,18 +3,18 @@ import { handleErrorWithSentry, sentryHandle } from "@sentry/sveltekit";
 import * as Sentry from '@sentry/sveltekit';
 import { ProfilingIntegration } from "@sentry/profiling-node";
 
-Sentry.init({
-  dsn: 'https://6396667f7493703c96bed4717a086c02@o4506125009813504.ingest.sentry.io/4506125012762624',
-  tracesSampleRate: 1.0,
-  profilesSampleRate: 1.0, // Profiling sample rate is relative to tracesSampleRate
-  integrations: [
-    // Add profiling integration to list of integrations
-    new ProfilingIntegration(),
-  ],
-});
+// Sentry.init({
+//   dsn: 'https://6396667f7493703c96bed4717a086c02@o4506125009813504.ingest.sentry.io/4506125012762624',
+//   tracesSampleRate: 1.0,
+//   profilesSampleRate: 1.0, // Profiling sample rate is relative to tracesSampleRate
+//   integrations: [
+//     // Add profiling integration to list of integrations
+//     new ProfilingIntegration(),
+//   ],
+// });
 
 // If you have custom handlers, make sure to place them after `sentryHandle()` in the `sequence` function.
-export const handle = sequence(sentryHandle());
-
-// If you have a custom error handler, pass it to `handleErrorWithSentry`
-export const handleError = handleErrorWithSentry();
+// export const handle = sequence(sentryHandle());
+//
+// // If you have a custom error handler, pass it to `handleErrorWithSentry`
+// export const handleError = handleErrorWithSentry();
