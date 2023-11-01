18bee6abb523206f1037eb8c02c95974ae910259
Steven R. Baker <steven@stevenrbaker.com>
2023-11-02 00:21:14 +0100

    Move things around.

--- frontend/src/lib/CWFGame.ts
+++ frontend/src/lib/CWFGame.ts
@@ -49,3 +49,99 @@ export const isGameComplete = (game) => {
 export const normalizedGameStreakCount = (game: NormalizedCWFGame) => {
   return fp.filter((result) => result, game.guessResults).length;
 };
+
+// non-normalized game
+
+export type SanityDocumentMetadata = {
+  [key: string]: any
+} & {
+  _id: string,
+  createdAt: string,
+}
+
+export type StreakGuessingGamePrompt = SanityDocumentMetadata & {
+  [key: string]: any
+} & {}
+
+export type StreakGuessingGameGuessResult = SanityDocumentMetadata & {
+  [key: string]: any
+} & {
+  challenge: StreakGuessingGameChallenge,
+  guesser?: StreakGuessingGamePlayer,
+  guesses: string[],
+  hintsUsed?: string[],
+  guessedCorrectly?: boolean,
+}
+
+export type StreakGuessingGamePlayer = SanityDocumentMetadata & {
+  [key: string]: any
+} & {}
+
+export type StreakGuessingGameDrawing = SanityDocumentMetadata & {
+  [key: string]: any
+} & {}
+
+export type StreakGuessingGameChallenge = SanityDocumentMetadata & {
+  [key: string]: any
+} & {
+  challenger: StreakGuessingGamePlayer,
+  prompt: StreakGuessingGamePrompt,
+  choodle?: StreakGuessingGameDrawing,
+}
+
+export type StreakGuessingGame = SanityDocumentMetadata & {
+  [key: string]: any
+} & {
+  currentChallenge: StreakGuessingGameChallenge,
+  player1: StreakGuessingGamePlayer,
+  player2?: StreakGuessingGamePlayer,
+  guessResults: StreakGuessingGameGuessResult[],
+}
+
+export const streakCount = (game: StreakGuessingGame): number => {
+  return fp.size(fp.filter(guessResult => guessResult.guessedCorrectly, game.guessResults))
+}
+
+export const createEmptyGameFromChallenge = (challenge: StreakGuessingGameChallenge): StreakGuessingGame => {
+  return {
+    _id: challenge._id,
+    createdAt: challenge.createdAt,
+    currentChallenge: {...challenge, challenger: challenge.challenger},
+    player1: challenge.challenger,
+    guessResults: [],
+  }
+}
+
+export const otherPlayer = (currentPlayer: StreakGuessingGamePlayer, game: StreakGuessingGame) => {
+  if (currentPlayer._id === game.player1._id && game.player2) return game.player2
+  return game.player1
+}
+
+export const whoseTurn = (game: StreakGuessingGame): StreakGuessingGamePlayer => {
+  if (!game.player2) return game.player1
+  if (!game.currentChallenge.choodle) return game.currentChallenge.challenger
+
+  const currentGuessResult = guessResultForCurrentChallenge(game)
+  if (!currentGuessResult) return otherPlayer(game.currentChallenge.challenger, game)
+  if (currentGuessResult?.guessedCorrectly === undefined) return otherPlayer(game.currentChallenge.challenger, game)
+
+  return otherPlayer(game.currentChallenge.challenger, game)
+}
+
+export const guessResultForCurrentChallenge = (game: StreakGuessingGame) => {
+  return fp.find(guessResult => guessResult.challenge._id === game.currentChallenge._id, game.guessResults)
+}
+
+export const whichAction = (game: StreakGuessingGame): string => {
+  if (!game.currentChallenge.choodle) return "draw"
+  if (!game.player2) return "share"
+
+  const currentGuessResult = guessResultForCurrentChallenge(game)
+  if (currentGuessResult && currentGuessResult.guessedCorrectly !== undefined) {
+    if (game.currentChallenge.prompt && !game.currentChallenge.choodle) return "draw"
+
+    return "pick"
+  }
+
+  return "guess"
+}
