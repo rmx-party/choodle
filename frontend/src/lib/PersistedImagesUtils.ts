a144c1242530c2245178fc81fbb01ca2c7826030
Brandon Dees <dees@pm.me>
2023-10-31 20:33:27 -0500

    tidy up

--- frontend/src/lib/PersistedImagesUtils.ts
+++ frontend/src/lib/PersistedImagesUtils.ts
@@ -1,12 +1,12 @@
 import imageUrlBuilder from '@sanity/image-url'
-import {readWriteClient} from "$lib/CMSUtils";
+import { readOnlyClient } from "$lib/CMSUtils";
 
 // Get a pre-configured url-builder from your sanity client
-export const builder = imageUrlBuilder(readWriteClient)
+export const builder = imageUrlBuilder(readOnlyClient)
 
 // Then we like to make a simple function like this that gives the
 // builder an image and returns the builder for you to specify additional
 // parameters:
 export function urlFor(source: string) {
-    return builder.image(source)
+  return builder.image(source)
 }
