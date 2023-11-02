ef4a58295ee1ebae40eead1171e72d6b0cb82d13
Steven R. Baker <steven@stevenrbaker.com>
2023-11-02 23:06:13 +0100

    Load CMS content in the layout.

--- frontend/src/routes/+layout.server.ts
+++ frontend/src/routes/+layout.server.ts
@@ -1,5 +1,9 @@
-import { env } from '$env/dynamic/private';
+import {env} from '$env/dynamic/private';
+import {cachedReadOnlyClient} from "$lib/CMSUtils";
 
 export function load() {
-  return { analyticsId: env.VERCEL_ANALYTICS_ID };
+  return {
+    analyticsId: env.VERCEL_ANALYTICS_ID,
+    copy: cachedReadOnlyClient.fetch(`*[_type == "choodleWithFriendsCopy"] | order(_createdAt) [0]`),
+  }
 }
