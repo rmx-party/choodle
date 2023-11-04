992030ff4cb7aabf8f610c66e4624d178b744d17
Brandon Dees <dees@pm.me>
2023-11-03 23:58:31 -0500

    looks like this is unneeded now

--- frontend/src/routes/game/cwf/draw/[[challengeId]]/+page.ts
+++ frontend/src/routes/game/cwf/draw/[[challengeId]]/+page.ts
@@ -1,12 +1,3 @@
-import { cachedReadOnlyClient } from '$lib/CMSUtils';
-import { loading } from '$lib/store';
-
 export async function load({ params }) {
-  loading.set(true);
-
-  return {
-    copy: cachedReadOnlyClient.fetch(
-      `*[_type == "choodleWithFriendsCopy"] | order(_createdAt) [0]`
-    ),
-  };
+  return {};
 }
