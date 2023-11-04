e073d9ce8e0a48ccc8f4e595946288bd4f31fb10
Brandon Dees <dees@pm.me>
2023-11-03 22:06:40 -0500

    loading store true by default seems to help with state flicker during page loads

--- frontend/src/lib/store.ts
+++ frontend/src/lib/store.ts
@@ -1,4 +1,4 @@
-import { writable } from 'svelte/store';
+import { writable } from "svelte/store";
 
 export type DialogState = {
   [domId: string]: boolean;
@@ -13,13 +13,13 @@ export const openDialog = (id) => switchDialog(id, true);
 export const closeDialog = (id) => switchDialog(id, false);
 
 export const tokenId = writable(0);
-export const loading = writable(false);
-export const loadingMessage = writable('enhancing happiness');
+export const loading = writable(true);
+export const loadingMessage = writable("enhancing happiness");
 export const isOnline = writable(true);
 
-export const imageData = writable('');
-export const nftTitle = writable('');
-export const nftAlt = writable('');
-export const nftDescription = writable('');
+export const imageData = writable("");
+export const nftTitle = writable("");
+export const nftAlt = writable("");
+export const nftDescription = writable("");
 
-export const filterState = writable('all');
+export const filterState = writable("all");
