03862a7884c411cddf79089763a8abfec01b3406
Brandon Dees <dees@pm.me>
2023-11-03 23:54:07 -0500

    svelte has a nice function to pre-fetch all the code to use in subsequent routes

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
-  import {browser, dev} from '$app/environment';
-  import {isOnline} from '$lib/store';
-  import {webVitals} from '$lib/vitals';
+  import { browser, dev } from '$app/environment';
+  import { isOnline } from '$lib/store';
+  import { webVitals } from '$lib/vitals';
   import '$lib/assets/fonts.css';
-  import {onMount} from 'svelte';
+  import { onMount } from 'svelte';
   import '../app.css';
   import LoadingIndicator from '../components/LoadingIndicator.svelte';
   import Bugsnag from '@bugsnag/js';
-  import {page} from '$app/stores';
-  import fp from "lodash/fp";
+  import { page } from '$app/stores';
+  import fp from 'lodash/fp';
+  import { preloadCode } from '$app/navigation';
 
   export let data;
 
@@ -22,14 +23,21 @@
   }
 
   if (browser && !dev) {
-    Bugsnag.start({apiKey: '91931cee1cbae5feec1925a566386158'});
+    Bugsnag.start({ apiKey: '91931cee1cbae5feec1925a566386158' });
   }
 
-  const rotatingMessages = fp.filter(msg => msg, [data.copy?.loadingMessage1, data.copy?.loadingMessage2, data.copy?.loadingMessage3, data.copy?.loadingMessage4, data.copy?.loadingMessage5])
+  const rotatingMessages = fp.filter(
+    (msg) => msg,
+    [
+      data.copy?.loadingMessage1,
+      data.copy?.loadingMessage2,
+      data.copy?.loadingMessage3,
+      data.copy?.loadingMessage4,
+      data.copy?.loadingMessage5,
+    ]
+  );
 
   onMount(async () => {
-    if (!browser) return;
-
     window.addEventListener('online', () => {
       console.log('online');
       isOnline.set(true);
@@ -38,8 +46,10 @@
       console.log('offline');
       isOnline.set(false);
     });
+
+    preloadCode('/game/cwf/pick', '/game/cwf/draw', '/game/cwf/share', '/game/cwf/guess');
   });
 </script>
 
-<LoadingIndicator {rotatingMessages}/>
-<slot/>
+<LoadingIndicator {rotatingMessages} />
+<slot />
