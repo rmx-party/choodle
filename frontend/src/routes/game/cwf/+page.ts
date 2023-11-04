f7963c8ce7fbd9b4edbb79705bbafb4ac89b769b
Brandon Dees <dees@pm.me>
2023-11-03 22:07:06 -0500

    copy loading made redundant by moving to layout loader

--- frontend/src/routes/game/cwf/+page.ts
+++ frontend/src/routes/game/cwf/+page.ts
@@ -1,13 +1,12 @@
-import { cachedReadOnlyClient, readOnlyClient } from '$lib/CMSUtils';
+import { readOnlyClient } from "$lib/CMSUtils";
 
 export async function load() {
   return {
-    copy: cachedReadOnlyClient.fetch(
-      `*[_type == "choodleWithFriendsCopy"] | order(_createdAt) [0]`
-    ),
     games: readOnlyClient.fetch(
-      `*[_type == "cwfgame"]{..., currentChallenge->{..., challenger->{...}}, player1->{...}, player2->{...}, guessResults[]->{..., challenge->{...}}} | order(_createdAt desc)`
+      `*[_type == "cwfgame"]{..., currentChallenge->{..., challenger->{...}}, player1->{...}, player2->{...}, guessResults[]->{..., challenge->{...}}} | order(_createdAt desc)`,
     ),
-    challenges: readOnlyClient.fetch(`*[_type == "challenge"]{..., challenger->{...}}`),
+    challenges: readOnlyClient.fetch(
+      `*[_type == "challenge"]{..., challenger->{...}}`,
+    ),
   };
 }
