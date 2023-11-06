7e7af114bcc6ba799de10816efa6b99587cdc4f3
Steven R. Baker <steven@stevenrbaker.com>
2023-11-06 22:48:13 +0100

    Add deletion for creators.

--- frontend/src/routes/game/cwf/admin/+page.svelte
+++ frontend/src/routes/game/cwf/admin/+page.svelte
@@ -1,18 +1,19 @@
 <script lang="ts">
   import Button from '../../../../components/Button.svelte';
-  import { readOnlyClient, readWriteClient } from '$lib/CMSUtils';
-  import { onMount } from 'svelte';
-  import { loading } from '$lib/store';
+  import {readOnlyClient, readWriteClient} from '$lib/CMSUtils';
+  import {onMount} from 'svelte';
+  import {loading} from '$lib/store';
 
   let gameCount = 0;
   let challengeCount = 0;
   let guessCount = 0;
   let pointCount = 0;
+  let creatorCount = 0;
 
   const deleteAllGames = async () => {
     const games = await readWriteClient.fetch('*[_type == "cwfgame"]');
     gameCount = games.length;
-    console.log({ gameCount });
+    console.log({gameCount});
 
     const challenges: any[] = await readOnlyClient.fetch('*[_type == "challenge"]');
     for (const challenge of challenges) {
@@ -20,7 +21,7 @@
     }
 
     for (const game of games) {
-      await readWriteClient.delete({ query: `*[references("${game._id}")]` });
+      await readWriteClient.delete({query: `*[references("${game._id}")]`});
       await readWriteClient.delete(game._id);
       gameCount--;
     }
@@ -29,9 +30,9 @@
   const deleteAllChallenges = async () => {
     const challenges: any[] = await readOnlyClient.fetch('*[_type == "challenge"]');
     challengeCount = challenges.length;
-    console.log({ challengeCount });
+    console.log({challengeCount});
     for (const challenge of challenges) {
-      await readWriteClient.delete({ query: `*[references("${challenge._id}")]` });
+      await readWriteClient.delete({query: `*[references("${challenge._id}")]`});
       await readWriteClient.delete(challenge._id);
       challengeCount--;
     }
@@ -40,10 +41,10 @@
   const deleteAllGuesses = async () => {
     const guesses = await readWriteClient.fetch('*[_type == "guess"]');
     guessCount = guesses.length;
-    console.log({ guessCount });
+    console.log({guessCount});
     for (const guess of guesses) {
       try {
-        await readWriteClient.delete({ query: `*[references("${guess._id}")]` });
+        await readWriteClient.delete({query: `*[references("${guess._id}")]`});
         await readWriteClient.delete(guess._id);
         guessCount--;
       } catch (e) {
@@ -55,11 +56,22 @@
   const deleteAllPoints = async () => {
     const points = await readWriteClient.fetch('*[_type == "points"]');
     pointCount = points.length;
-    console.log({ points });
+    console.log({points});
     for (const point of points) {
-      await readWriteClient.delete({ query: `*[references("${point._id}")]` });
+      await readWriteClient.delete({query: `*[references("${point._id}")]`});
       await readWriteClient.delete(point._id);
       pointCount--;
+    }
+  };
+
+  const deleteAllCreators = async () => {
+    const creators = await readWriteClient.fetch('*[_type == "creator"]');
+    creatorCount = creators.length;
+    console.log({creators});
+    for (const creator of creators) {
+      await readWriteClient.delete({query: `*[references("${creator._id}")]`});
+      await readWriteClient.delete(creator._id);
+      creatorCount--;
     }
   };
 
@@ -73,4 +85,5 @@
   <Button on:click={deleteAllChallenges}>Delete All {challengeCount} Challenges</Button>
   <Button on:click={deleteAllGuesses}>Delete All {guessCount} Guesses</Button>
   <Button on:click={deleteAllPoints}>Delete All {pointCount} Points</Button>
+  <Button on:click={deleteAllCreators}>Delete All {creatorCount} Creators</Button>
 </div>
