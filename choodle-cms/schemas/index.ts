322a1f0b05c22276d0b7c5226a8141aa274d90ae
Steven R. Baker <steven@stevenrbaker.com>
2023-11-07 20:24:12 +0100

    Delete unused schemas.

--- choodle-cms/schemas/index.ts
+++ choodle-cms/schemas/index.ts
@@ -1,29 +1,17 @@
-import dailyPrompt from './dailyPrompt';
 import choodle from "./choodle";
-import howto from "./howto";
-import viewChoodle from "./viewChoodle";
-import certificateEmail from "./certificateEmail";
-import certificateModal from "./certificateModal";
 import gamePrompt from './gamePrompt';
 import choodleWithFriendsCopy from "./choodleWithFriendsCopy";
 import creator from './creator';
 import challenge from './challenge';
-import points from "./points";
 import guess from "./guess";
 import cwfgame from "./cwfgame";
 
 export const schemaTypes = [
-  dailyPrompt,
   choodle,
-  howto,
-  viewChoodle,
-  certificateEmail,
-  certificateModal,
   gamePrompt,
   choodleWithFriendsCopy,
   creator,
   challenge,
   guess,
-  points,
   cwfgame,
 ]
