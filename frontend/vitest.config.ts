a8eedee8d271ef38a26131c539feeae4c707d426
Steven R. Baker <steven@stevenrbaker.com>
2023-11-07 01:09:22 +0100

    Fix testing config.

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,11 +1,9 @@
 import {defineConfig} from 'vitest/config'
-import {svelte} from '@sveltejs/vite-plugin-svelte'
-import {sveltekit} from "@sveltejs/kit/vite";
+import {sveltekit} from '@sveltejs/kit/vite'
 
 export default defineConfig({
   plugins: [
-    svelte({hot: !process.env.VITEST}),
-    sveltekit(),
+    sveltekit({hot: !process.env.VITEST}),
   ],
   test: {
     globals: true,
