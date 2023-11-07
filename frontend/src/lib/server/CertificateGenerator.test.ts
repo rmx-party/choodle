5f61c4ee3b76a84ad26073484dc551ea91a1a709
Steven R. Baker <steven@stevenrbaker.com>
2023-11-07 19:41:22 +0100

    Get rid of certificate stuff.

--- frontend/src/lib/server/CertificateGenerator.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, expect, it } from 'vitest';
-
-import { generateCertificateFor } from '$lib/server/CertificateGenerator';
-
-describe('load an image', () => {
-  it('loads the correct file by checking its size', async () => {
-    await generateCertificateFor('nylCqCwRnhr26khFEUw8oj');
-  });
-});
