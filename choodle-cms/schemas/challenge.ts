23d06ce18c25e3624fab036a01653239bdc6fe7d
Brandon Dees <dees@pm.me>
2023-11-08 12:09:04 -0600

    Some tidying of sanity labels and slots

--- choodle-cms/schemas/challenge.ts
+++ choodle-cms/schemas/challenge.ts
@@ -1,30 +1,31 @@
 export default {
-  name: 'challenge',
-  type: 'document',
-  title: 'Choodle w/ Friends Challenge',
+  name: "challenge",
+  type: "document",
+  title: "Challenge",
   fields: [
     {
-      name: 'gameRef', // FIXME: migrate this to game
-      type: 'reference',
-      to: {type: 'cwfgame'}
+      name: "gameRef", // FIXME: migrate this to game
+      type: "reference",
+      to: { type: "cwfgame" },
     },
     {
-      name: 'choodle',
-      title: 'A choodle drawing based on a word prompt for someone to try to guess',
-      type: 'reference',
-      to: {type: 'choodle'}
+      name: "choodle",
+      title:
+        "A choodle drawing based on a word prompt for someone to try to guess",
+      type: "reference",
+      to: { type: "choodle" },
     },
     {
-      name: 'gamePrompt', // FIXME: migrate this to gamePrompt
-      title: 'the prompt this challenge choodle is based on',
-      type: 'reference',
-      to: {type: 'gamePrompt'}
+      name: "gamePrompt",
+      title: "the prompt this challenge choodle is based on",
+      type: "reference",
+      to: { type: "gamePrompt" },
     },
     {
-      name: 'challenger',
-      title: 'the person who drew the choodle to be guessed',
-      type: 'reference',
-      to: {type: 'creator'}
+      name: "challenger",
+      title: "the person who drew the choodle to be guessed",
+      type: "reference",
+      to: { type: "creator" },
     },
-  ]
-}
+  ],
+};
