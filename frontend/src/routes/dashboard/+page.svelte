d1376c16bc70e3ddf2fb0b6bf4ff26a41a00f654
Steven R. Baker <steven@stevenrbaker.com>
2023-11-08 19:51:44 +0100

    Delete all of the my turn/your turn stuff that isn't needed anymore.

--- frontend/src/routes/dashboard/+page.svelte
+++ frontend/src/routes/dashboard/+page.svelte
@@ -7,50 +7,25 @@
   import {page} from '$app/stores';
   import MetaData from '../../components/MetaData.svelte';
   import {pageBackgroundDefault} from '$lib/Configuration';
-  import {writable} from 'svelte/store';
   import fp from 'lodash/fp';
-  import {toHTML} from '@portabletext/to-html';
   import {loading} from '$lib/store';
-  import {urlFor} from '$lib/PersistedImagesUtils';
   import type {
     SanityDocumentMetadata,
     StreakGuessingGame,
     StreakGuessingGamePlayer,
   } from '$lib/CWFGame';
-  import {isGameComplete, isPlayerInGame, whoseTurn} from '$lib/CWFGame';
+  import {isPlayerInGame} from '$lib/CWFGame';
   import DashboardGameEntry from '../../components/DashboardGameEntry.svelte';
-  import {GameBuilder} from '$lib/GameBuilder';
-  import PixelImage from '../../components/PixelImage.svelte';
-  import {fly} from 'svelte/transition';
 
   loading.set(true);
+
   export let data;
+
   let currentChoodler: StreakGuessingGamePlayer;
-
   let myGames: StreakGuessingGame[] = [];
-
-  const navItems = ['my games', 'rules  '];
-  let activeTab = writable(navItems[0]);
 
   const startGame = async () => {
     await goto(`/`);
-  };
-
-  let myTurnGames: StreakGuessingGame[] = [];
-  let theirTurnGames: StreakGuessingGame[] = [];
-  $: [myTurnGames, theirTurnGames] = fp.partition(isMyTurn, myGames);
-
-  let challengesInMyGames;
-  let currentPlayerChallenges;
-
-  const challengesInGame = (game: StreakGuessingGame) => {
-    return fp.map((guessResult) => guessResult.challenge, game.guessResults);
-  };
-
-  const isMyTurn = (game) => {
-    if (!game.player2) return game.player2;
-
-    return whoseTurn(game)._id === currentChoodler._id;
   };
 
   const sortedByCreatedAt = (
@@ -83,33 +58,6 @@
     );
     console.log(`myGames`, myGames);
 
-    challengesInMyGames = fp.flatMapDeep(challengesInGame, myGames);
-    currentPlayerChallenges = fp.filter(
-      (challenge) => challenge.challenger?._id === currentChoodler._id,
-      data.challenges
-    );
-
-    const unAnsweredChallenges = fp.uniqBy(
-      '_id',
-      fp.differenceBy((challenge) => challenge._id, currentPlayerChallenges, challengesInMyGames)
-    );
-
-    // only challenges where there is no gameRef set
-    const unAnsweredChallengesWithoutGameRef = fp.filter(
-      (challenge) => !challenge.gameRef,
-      unAnsweredChallenges
-    );
-
-    const unAnsweredGames = fp.map(
-      (builder) => builder.build,
-      fp.map(GameBuilder.fromChallenge, unAnsweredChallengesWithoutGameRef)
-    );
-    myGames = fp.reject(
-      isGameComplete,
-      fp.uniqBy('_id', sortedByCreatedAt([...unAnsweredGames, ...myGames]))
-    );
-    // myGames = fp.uniqBy('_id', sortedByCreatedAt([...myGames]));
-
     loading.set(false);
   });
 </script>
@@ -117,66 +65,27 @@
 <MetaData title={data.copy.defaultPageTitle} themeColor={pageBackgroundDefault} url={$page.url}/>
 
 <LayoutContainer>
-  <div>
-    <PixelImage src={urlFor(data.copy.logo).url()} width="80%" alt=""/>
-  </div>
-  <section class="nav-and-hud">
-    <nav>
-      {#each navItems as navItem}
-          <span
-            on:click={() => {
-              activeTab.set(navItem);
-            }}
-            class={`${navItem == $activeTab ? 'active' : ''}`}
-          >
-            {navItem}
-          </span>
-      {/each}
-    </nav>
-
+  <section class="hud">
     <span class="username">{currentChoodler?.username || "unnamed user"}</span>
   </section>
 
-  {#if $activeTab === 'my games'}
-    <section class="tabContent my-games" in:fly={{ x: -2000, y: 0, duration: 200 }}>
-      <span class="game-list-heading">Your Turn</span>
-      {#each myTurnGames as myTurnGame (myTurnGame._id)}
-        <DashboardGameEntry
-          {currentChoodler}
-          game={myTurnGame}
-          gameListUserUnknownText={data.copy.gameListUserUnknownText}
-        />
-      {:else}
-        <div class="centre-container">
-          <p>All caught up</p>
-        </div>
-      {/each}
-      <div class="centre-container">
-        <Button
-          variant="secondary"
-          colour="yellow"
-          on:click={startGame}
-          style="margin: 1rem auto; flex-grow: 0;"
-        >
-          {data.copy.startGameButtonText}
-        </Button>
-      </div>
-      <span class="game-list-heading">Their Turn</span>
-      {#each theirTurnGames as theirTurnGame (theirTurnGame._id)}
-        <DashboardGameEntry
-          {currentChoodler}
-          game={theirTurnGame}
-          gameListUserUnknownText={data.copy.gameListUserUnknownText}
-        />
-      {/each}
-    </section>
-  {/if}
+  <div class="centre-container">
+    <Button variant="secondary" colour="yellow" on:click={startGame} style="margin: 1rem auto; flex-grow: 0;">
+      {data.copy.startGameButtonText}
+    </Button>
+  </div>
 
-  {#if $activeTab === 'rules'}
-    <section class="tabContent my-games" in:fly={{ x: 2000, y: 0, duration: 200 }}>
-      {@html toHTML(data.copy.rules_content)}
-    </section>
-  {/if}
+  {#each myGames as myGame (myGame._id)}
+    <DashboardGameEntry
+      {currentChoodler}
+      game={myGame}
+      gameListUserUnknownText={data.copy.gameListUserUnknownText}
+    />
+  {:else}
+    <div class="centre-container">
+      <p>All caught up</p>
+    </div>
+  {/each}
 </LayoutContainer>
 
 <style>
@@ -186,37 +95,13 @@
     justify-content: center;
   }
 
-  .nav-and-hud {
+  .hud {
     display: flex;
     flex-direction: row;
     align-items: flex-start;
     justify-content: space-between;
     width: 100%;
-  }
-
-  nav {
-    align-self: flex-start;
-    display: block;
-    margin: 1rem 0 2rem;
-    text-align: left;
-
-    text-transform: capitalize;
-    font-size: 18px;
-    font-family: 'DejaVu Sans Bold';
-    color: darkgrey;
-  }
-
-  nav > span {
-    display: block;
-    width: 100%;
-  }
-
-  nav > .active {
-    color: var(--choodle-black);
-  }
-
-  nav > span + span {
-    margin-top: 1rem;
+    margin-top: 2rem;
   }
 
   .username {
@@ -231,40 +116,5 @@
     font-style: normal;
     font-weight: 700;
     line-height: 120%; /* 1.05rem */
-  }
-
-  .tabContent {
-    display: block;
-    width: 100%;
-    text-align: left;
-  }
-
-  .game-list-heading {
-    /* mobile/caption-bold */
-    font-family: DejaVu Sans Bold;
-    font-size: 14px;
-    font-style: normal;
-    font-weight: bold;
-    line-height: 120%; /* 16.8px */
-    display: block;
-    width: 100%;
-    border-bottom: 1px solid var(--choodle-black);
-    padding: 0.5rem 0;
-  }
-
-  .highlight {
-    background: var(--choodle-yellow);
-  }
-
-  .won {
-    color: hsla(108, 90%, 28%, 1);
-  }
-
-  .lost {
-    color: hsla(0, 100%, 21%, 1);
-  }
-
-  .status {
-    text-align: center;
   }
 </style>
