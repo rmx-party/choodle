bff877bcf03e1bd3765a6dca416ef6762e281a41
Steven R. Baker <steven@stevenrbaker.com>
2023-11-08 23:04:33 +0100

    404 when a challenge id is provided but not found

--- frontend/src/routes/[[challengeId]]/+page.ts
+++ frontend/src/routes/[[challengeId]]/+page.ts
@@ -1,10 +1,25 @@
-import { cachedReadOnlyClient } from "$lib/CMSUtils";
-import fp from "lodash/fp.js";
+import { cachedReadOnlyClient, readOnlyClient } from '$lib/CMSUtils'
+import fp from 'lodash/fp.js'
+import { error } from '@sveltejs/kit'
 
 export async function load({ params }) {
+  let challenge
+  if (params.challengeId) {
+    if (!params.challengeId.startsWith('challenge-')) {
+      throw error(404)
+    }
+
+    challenge = await readOnlyClient.fetch(
+      `*[_type == "challenge" && _id == "${params.challengeId}"][0]`
+    )
+
+    if (!challenge) {
+      throw error(404)
+    }
+  }
+
   return {
-    records: fp.shuffle(
-      await cachedReadOnlyClient.fetch(`*[_type == "gamePrompt"]`),
-    ),
-  };
+    records: fp.shuffle(await cachedReadOnlyClient.fetch(`*[_type == "gamePrompt"]`)),
+    challenge,
+  }
 }
