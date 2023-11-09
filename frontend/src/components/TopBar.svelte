f04f8b123ead9f8abdde40f21de30d726d8bf565
Brandon Dees <dees@pm.me>
2023-11-09 11:38:36 -0600

    reduce the vertical space required by topbar

--- frontend/src/components/TopBar.svelte
+++ frontend/src/components/TopBar.svelte
@@ -1,15 +1,14 @@
 <div class="topBar-layout" {...$$restProps}>
-  <slot name="topBarContent"/>
+  <slot name="topBarContent" />
 </div>
 
 <style>
   .topBar-layout {
-    min-height: 3.1875rem;
     width: 100%;
 
     display: flex;
     flex-direction: row;
-    padding: 0.5rem 1rem 0.75rem 1rem;
+    padding: 0.5rem 1rem;
     align-items: center;
     justify-content: center;
     gap: 0.625rem;
