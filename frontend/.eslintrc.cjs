b54b7796f3924befd86c1227b2bfdac700640763
Brandon Dees <dees@pm.me>
2023-11-09 02:58:30 -0600

    lodash-fp linting plugin

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,17 +1,18 @@
 module.exports = {
   root: true,
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:svelte/recommended',
-    'prettier',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:lodash-fp/recommended",
+    "plugin:svelte/recommended",
+    "prettier",
   ],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
   parserOptions: {
-    sourceType: 'module',
+    sourceType: "module",
     ecmaVersion: 2020,
-    extraFileExtensions: ['.svelte'],
+    extraFileExtensions: [".svelte"],
   },
   env: {
     browser: true,
@@ -20,10 +21,10 @@ module.exports = {
   },
   overrides: [
     {
-      files: ['*.svelte'],
-      parser: 'svelte-eslint-parser',
+      files: ["*.svelte"],
+      parser: "svelte-eslint-parser",
       parserOptions: {
-        parser: '@typescript-eslint/parser',
+        parser: "@typescript-eslint/parser",
       },
     },
   ],
