ee0e71c713b6c56dcfee5dcffdd5753ec9870648
Brandon Dees <dees@pm.me>
2023-11-08 21:43:11 -0600

    handle challenge not found on share page

--- frontend/src/routes/share/[id]/+page.ts
+++ frontend/src/routes/share/[id]/+page.ts
@@ -5,6 +5,9 @@ export const load: PageLoad = async ({ params }) => {
   return {
     challenge: readOnlyClient.fetch(
       `*[_type == "challenge" && _id == "${params.id}"]{..., challenger->{...}, choodle->{...}, gamePrompt->{...}} [0]`,
-    ),
+    ).catch((e) => {
+      console.error(e);
+      throw Error(404);
+    }),
   };
 };
