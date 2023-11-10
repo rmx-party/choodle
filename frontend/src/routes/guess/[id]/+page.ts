937b944a69b9d69d05e27d7932d871f28ce5c107
Brandon Dees <dees@pm.me>
2023-11-10 15:13:20 -0600

    try using long expiration in ISR mode routes

--- frontend/src/routes/guess/[id]/+page.ts
+++ frontend/src/routes/guess/[id]/+page.ts
@@ -1,10 +1,13 @@
-import { PUBLIC_ISR_BYPASS_TOKEN } from "$env/static/public";
+import {
+  PUBLIC_ISR_BYPASS_TOKEN,
+  PUBLIC_ISR_EXPIRATION_SECONDS,
+} from "$env/static/public";
 import { cachedReadOnlyClient } from "$lib/CMSUtils";
 import type { PageLoad } from "./$types";
 
 export const config = {
   isr: {
-    expiration: 600,
+    expiration: PUBLIC_ISR_EXPIRATION_SECONDS || 600,
     bypassToken: PUBLIC_ISR_BYPASS_TOKEN,
   },
 };
--- frontend/src/routes/share/[id]/+page.ts
+++ frontend/src/routes/share/[id]/+page.ts
@@ -1,5 +1,16 @@
+import {
+  PUBLIC_ISR_BYPASS_TOKEN,
+  PUBLIC_ISR_EXPIRATION_SECONDS,
+} from "$env/static/public";
 import { readOnlyClient } from "$lib/CMSUtils";
 import type { PageLoad } from "./$types";
+
+export const config = {
+  isr: {
+    expiration: PUBLIC_ISR_EXPIRATION_SECONDS || 600,
+    bypassToken: PUBLIC_ISR_BYPASS_TOKEN,
+  },
+};
 
 export const load: PageLoad = async ({ params }) => {
   return {
