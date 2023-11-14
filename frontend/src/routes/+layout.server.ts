b20a9c520cc37ddc47c2d3ead671e52bc8c9000d
Brandon Dees <dees@pm.me>
2023-11-14 17:55:33 -0600

    layout load type

--- frontend/src/routes/+layout.server.ts
+++ frontend/src/routes/+layout.server.ts
@@ -1,9 +1,12 @@
-import {env} from '$env/dynamic/private';
-import {cachedReadOnlyClient} from "$lib/CMSUtils";
+import { env } from "$env/dynamic/private";
+import { cachedReadOnlyClient } from "$lib/CMSUtils";
+import type { LayoutServerLoad } from "./$types";
 
-export function load() {
+export const load: LayoutServerLoad = () => {
   return {
     analyticsId: env.VERCEL_ANALYTICS_ID,
-    copy: cachedReadOnlyClient.fetch(`*[_type == "choodleWithFriendsCopy"] | order(_createdAt) [0]`),
-  }
-}
+    copy: cachedReadOnlyClient.fetch(
+      `*[_type == "choodleWithFriendsCopy"] | order(_createdAt) [0]`,
+    ),
+  };
+};
