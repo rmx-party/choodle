e46139e8e04272862f6e7c840b42f59d72f9b080
Brandon Dees <dees@pm.me>
2023-11-16 12:33:38 -0600

    extract welcome message component since it's used in a couple places, with design adjustments

---
+++ frontend/src/components/UserWelcomeMessage.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import type { StreakGuessingGamePlayer } from '$lib/CWFGame'
+  import { getContext } from 'svelte'
+  import type { Writable } from 'svelte/store'
+
+  const currentChoodler: Writable<StreakGuessingGamePlayer> = getContext('choodler')
+</script>
+
+<h3 class="hud">
+  <strong>
+    {#if $currentChoodler?.username?.length}
+      👋 Hi <span class="username">{$currentChoodler?.username || 'unnamed user'}</span>!
+    {:else}
+      👋 Hi!
+    {/if}
+    <br />
+    Welcome Home!
+  </strong>
+</h3>
+
+<style>
+  .hud {
+    width: 100%;
+    margin-top: 2rem;
+  }
+
+  .username {
+    display: inline-block;
+    color: var(--text-text-primary, #141518);
+    background: var(--choodle-yellow, #fef40a);
+  }
+</style>
