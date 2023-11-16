322d566f3fb6cfc81065600e805cc4b2848dd6c6
Brandon Dees <dees@pm.me>
2023-11-16 13:49:20 -0600

    comment to clarify ambiguous name without renaming yet

--- frontend/src/lib/Configuration.ts
+++ frontend/src/lib/Configuration.ts
@@ -1,14 +1,14 @@
-import type { Dimensiony } from '$lib/Calculations'
+import type { Dimensiony } from "$lib/Calculations";
 
-export const lineWidth = 1
-export const targetMaxSize: Dimensiony = { x: 195, y: 260 }
-export const drawColor = '#000000'
-export const backgroundColour = '#FFFFFF'
-export const pageBackgroundDefault = '#F7F7F7'
-export const choodleYellow = '#FEF40A'
-export const choodleUndoKey = 'choodle-undo'
-export const choodlePromptKey = 'choodle-selected-prompt'
-export const choodleCreatorIdKey = 'choodle-creator-id'
-export const choodlePersistedCreatorIdKey = 'choodle-persisted-creator-id'
-export const blackWhiteThreshold = 250
-export const upScaledImageRatio = 8
+export const lineWidth = 1;
+export const targetMaxSize: Dimensiony = { x: 195, y: 260 };
+export const drawColor = "#000000";
+export const backgroundColour = "#FFFFFF"; // NOTE: this is canvas background, not page bg
+export const pageBackgroundDefault = "#F7F7F7";
+export const choodleYellow = "#FEF40A";
+export const choodleUndoKey = "choodle-undo";
+export const choodlePromptKey = "choodle-selected-prompt";
+export const choodleCreatorIdKey = "choodle-creator-id";
+export const choodlePersistedCreatorIdKey = "choodle-persisted-creator-id";
+export const blackWhiteThreshold = 250;
+export const upScaledImageRatio = 8;
