a5cc2cbbe81121ef63ba80f8bae6edc99440f798
Brandon Dees <dees@pm.me>
2023-11-16 19:36:40 -0600

    try to get clearer version numbering

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,13 +1,15 @@
-import adapter from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/kit/vite';
-import * as child_process from 'child_process';
+import adapter from "@sveltejs/adapter-vercel";
+import { vitePreprocess } from "@sveltejs/kit/vite";
+import * as child_process from "child_process";
 
 let name;
 
-if (process.env.VERCEL === '1') {
-  name = process.env.VERCEL_GIT_COMMIT_SHA;
+if (process.env.VERCEL === "1") {
+  name =
+    `${process.env.npm_package_version}.ref-${process.env.VERCEL_GIT_COMMIT_SHA}`;
 } else {
-  name = child_process.execSync('git rev-parse HEAD').toString().trim();
+  name = `${process.env.npm_package_version}.ref-${child_process.execSync("git rev-parse HEAD").toString().trim()
+    }`;
 }
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -18,11 +20,11 @@ const config = {
 
   kit: {
     adapter: adapter({
-      runtime: 'nodejs18.x',
-      external: ['$app/stores'],
+      runtime: "nodejs18.x",
+      external: ["$app/stores"],
     }),
     version: {
-      name: name,
+      name,
     },
   },
 };
