1efc7dcf2dfd8e37181680fdbb982cf662360f7d
Brandon Dees <dees@pm.me>
2023-11-16 23:27:56 -0600

    can't do any image processing stuff on url builder if we want gifs

--- frontend/src/lib/PersistedImagesUtils.ts
+++ frontend/src/lib/PersistedImagesUtils.ts
@@ -1,5 +1,6 @@
 import imageUrlBuilder from "@sanity/image-url";
 import { cachedReadOnlyClient } from "$lib/CMSUtils";
+import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
 // Get a pre-configured url-builder from your sanity client
 export const builder = imageUrlBuilder(cachedReadOnlyClient);
@@ -7,6 +8,6 @@ export const builder = imageUrlBuilder(cachedReadOnlyClient);
 // Then we like to make a simple function like this that gives the
 // builder an image and returns the builder for you to specify additional
 // parameters:
-export const urlFor = (source: string) => {
-  return builder.image(source).auto("format");
+export const urlFor = (source: SanityImageSource) => {
+  return builder.image(source);
 };
