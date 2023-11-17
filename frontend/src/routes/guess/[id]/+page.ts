8bf6493af7e19c4c07e91a33f5ec8ce233a34ef4
Brandon Dees <dees@pm.me>
2023-11-17 16:33:23 -0600

    parameterize guess page query, add error capture, and no longer destructure the challenge data

--- frontend/src/routes/guess/[id]/+page.ts
+++ frontend/src/routes/guess/[id]/+page.ts
@@ -14,12 +14,16 @@ export const config = {
 
 export const load: PageLoad = async ({ params }) => {
   const challenge = await cachedReadOnlyClient.fetch(
-    `*[_type == "challenge" && _id == "${params.id}"]{..., challenger->{...}, choodle->{...}, gamePrompt->{...}} [0]`,
-  );
+    `*[_type == "challenge" && _id == $challengeId]{..., challenger->{...}, choodle->{...}, gamePrompt->{...}} [0]`,
+    {
+      challengeId: params.id,
+    },
+  ).catch((error) => {
+    console.error(`load failure`, error);
+    throw new error(404, `cms load failure for challenge id ${params.id}`);
+  });
 
   return {
     challenge,
-    gamePrompt: challenge.gamePrompt,
-    choodle: challenge.choodle,
   };
 };
