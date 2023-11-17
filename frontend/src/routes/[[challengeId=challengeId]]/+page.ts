5c199b43ba8280b45661ace87de3d6482ec593b0
Brandon Dees <dees@pm.me>
2023-11-17 17:01:50 -0600

    fix type import

--- frontend/src/routes/[[challengeId=challengeId]]/+page.ts
+++ frontend/src/routes/[[challengeId=challengeId]]/+page.ts
@@ -1,30 +1,30 @@
-import { cachedReadOnlyClient, readOnlyClient } from '$lib/CMSUtils'
-import { error } from '@sveltejs/kit'
+import { cachedReadOnlyClient, readOnlyClient } from "$lib/CMSUtils";
+import { error } from "@sveltejs/kit";
 
-import { PUBLIC_ISR_BYPASS_TOKEN } from '$env/static/public'
-import type { PageLoad } from '../../../../.svelte-kit/types/src/routes'
+import { PUBLIC_ISR_BYPASS_TOKEN } from "$env/static/public";
+import type { PageLoad } from "./$types";
 
 export const config = {
   isr: {
     expiration: 1440,
     bypassToken: PUBLIC_ISR_BYPASS_TOKEN,
   },
-}
+};
 
 export const load: PageLoad = async ({ params }) => {
-  let challenge
+  let challenge;
   if (params.challengeId) {
     challenge = await readOnlyClient.fetch(
-      `*[_type == "challenge" && _id == "${params.challengeId}"][0]`
-    )
+      `*[_type == "challenge" && _id == "${params.challengeId}"][0]`,
+    );
 
     if (!challenge) {
-      throw error(404)
+      throw error(404);
     }
   }
 
   return {
     records: cachedReadOnlyClient.fetch(`*[_type == "gamePrompt"]`),
     challenge,
-  }
-}
+  };
+};
