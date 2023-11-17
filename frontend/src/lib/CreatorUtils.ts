ad3f2130ef1a8a391881fe536d1096972a7ce12d
Brandon Dees <dees@pm.me>
2023-11-17 14:30:23 -0600

    find out when locate creator fails

--- frontend/src/lib/CreatorUtils.ts
+++ frontend/src/lib/CreatorUtils.ts
@@ -1,47 +1,52 @@
-import { browser } from '$app/environment'
-import localforage from 'localforage'
-import { choodleCreatorIdKey } from '$lib/Configuration'
-import { readOnlyClient, readWriteClient } from '$lib/CMSUtils'
+import { browser } from "$app/environment";
+import localforage from "localforage";
+import { choodleCreatorIdKey } from "$lib/Configuration";
+import { readOnlyClient, readWriteClient } from "$lib/CMSUtils";
 
 export async function getDeviceId(): Promise<string> {
-  if (!browser) return
+  if (!browser) return;
   try {
-    const existingId = await localforage.getItem(choodleCreatorIdKey)
+    const existingId = await localforage.getItem(choodleCreatorIdKey);
     if (existingId && existingId.length > 1) {
-      return existingId
+      return existingId;
     }
 
-    const uuid = window.crypto.randomUUID()
-    await localforage.setItem(choodleCreatorIdKey, uuid)
-    return uuid
+    const uuid = window.crypto.randomUUID();
+    await localforage.setItem(choodleCreatorIdKey, uuid);
+    return uuid;
   } catch (e) {
-    console.error(`getCreatorId failure, returning 'unknown'`, e)
-    return 'unknown'
+    console.error(`getCreatorId failure, returning 'unknown'`, e);
+    return "unknown";
   }
 }
 
-export const locateCreator = async ({ deviceId }: { deviceId?: string | undefined }) => {
+export const locateCreator = async (
+  { deviceId }: { deviceId?: string | undefined },
+) => {
   if (!deviceId) {
-    deviceId = await getDeviceId()
+    deviceId = await getDeviceId().catch((error) => {
+      uncaughtErrors.update((errors) => [...errors, error]);
+      return undefined;
+    });
   }
-  console.log(`locateCreator: ${deviceId}`)
+  console.log(`locateCreator: ${deviceId}`);
   let creator = await readOnlyClient.fetch(
     `*[_type == "creator" && deviceIds match $deviceId][0]`,
-    { deviceId }
-  )
-  console.log(`locatedCreator: ${creator}`)
+    { deviceId },
+  );
+  console.log(`locatedCreator: ${creator}`);
 
   if (!creator) {
     creator = await readWriteClient.create(
       {
-        _type: 'creator',
+        _type: "creator",
         deviceIds: [deviceId],
         choodles: [],
       },
-      { autoGenerateArrayKeys: true }
-    )
+      { autoGenerateArrayKeys: true },
+    );
   }
 
-  console.log({ creator })
-  return creator
-}
+  console.log({ creator });
+  return creator;
+};
