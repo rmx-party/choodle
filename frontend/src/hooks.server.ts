0f98b98c2ccbe71dd5970b92686b3c4caa809be8
Brandon Dees <dees@pm.me>
2023-11-17 02:00:28 -0600

    see if we can re-enable server side sentry features

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -1,20 +1,21 @@
-// import { sequence } from "@sveltejs/kit/hooks";
-// import { handleErrorWithSentry, sentryHandle } from "@sentry/sveltekit";
-// import * as Sentry from '@sentry/sveltekit';
+import { sequence } from "@sveltejs/kit/hooks";
+import { handleErrorWithSentry, sentryHandle } from "@sentry/sveltekit";
+import * as Sentry from "@sentry/sveltekit";
 // import { ProfilingIntegration } from "@sentry/profiling-node";
 
-// Sentry.init({
-//   dsn: 'https://6396667f7493703c96bed4717a086c02@o4506125009813504.ingest.sentry.io/4506125012762624',
-//   tracesSampleRate: 1.0,
-//   profilesSampleRate: 1.0, // Profiling sample rate is relative to tracesSampleRate
-//   // integrations: [
-//   //   // Add profiling integration to list of integrations
-//   //   new ProfilingIntegration(),
-//   // ],
-// });
+Sentry.init({
+  dsn:
+    "https://6396667f7493703c96bed4717a086c02@o4506125009813504.ingest.sentry.io/4506125012762624",
+  tracesSampleRate: 1.0,
+  profilesSampleRate: 1.0, // Profiling sample rate is relative to tracesSampleRate
+  // integrations: [
+  //   // Add profiling integration to list of integrations
+  //   new ProfilingIntegration(),
+  // ],
+});
 //
 // // If you have custom handlers, make sure to place them after `sentryHandle()` in the `sequence` function.
-// export const handle = sequence(sentryHandle());
+export const handle = sequence(sentryHandle());
 //
 // // If you have a custom error handler, pass it to `handleErrorWithSentry`
-// export const handleError = handleErrorWithSentry();
+export const handleError = handleErrorWithSentry();
