72527fee7e8a2b8de98e25a40b3888c25151a542
Brandon Dees <dees@pm.me>
2023-12-05 12:55:31 -0600

    don't load user in regular loaders because we want CDN caching

--- frontend/src/routes/+layout.server.ts
+++ frontend/src/routes/+layout.server.ts
@@ -1,14 +1,12 @@
-import { env } from "$env/dynamic/private";
+import { VERCEL_ANALYTICS_ID } from "$env/static/private";
 import { cachedReadOnlyClient } from "$lib/CMSUtils";
 import type { LayoutServerLoad } from "./$types";
 
 export const load: LayoutServerLoad = async ({ locals }) => {
-  const user = locals?.user || null;
   return {
-    analyticsId: env.VERCEL_ANALYTICS_ID,
+    analyticsId: VERCEL_ANALYTICS_ID,
     copy: cachedReadOnlyClient.fetch(
       `*[_type == "choodleWithFriendsCopy"] | order(_createdAt) [0]`,
     ),
-    user,
   };
 };
