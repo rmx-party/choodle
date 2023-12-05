8f5d624e559eb1cacb17b80b528ed018c8fa096e
Brandon Dees <dees@pm.me>
2023-12-05 14:37:27 -0600

    404 when challenge can't be found, better

--- frontend/src/routes/guess/[id=integer]/+page.server.ts
+++ frontend/src/routes/guess/[id=integer]/+page.server.ts
@@ -17,7 +17,7 @@ export const config = {
 const slug = "guess";
 
 export const load: PageServerLoad = async ({ params }) => {
-  const challengeId = params.id; // TODO: use challengeId with param matcher instead of id
+  const challengeId = Number(params.id);
   if (!challengeId) {
     throw error(404, `challengeId is required`);
   }
@@ -26,17 +26,22 @@ export const load: PageServerLoad = async ({ params }) => {
     `*[_type == "pageContent" && pageSlug == $slug][0]`,
     { slug },
   ).catch((error) => {
-    console.error(`load failure`, error); // TODO: craft some DRY handlers for this type stuff, distinguish between client mode and server mode, etc.
+    console.error(`cms load failure`, error); // TODO: craft some DRY handlers for this type stuff, distinguish between client mode and server mode, etc.
     throw new error(404, `cms load failure for pageContent slug ${slug}`);
   });
 
-  const challenge = await findChallenge({ id: Number(challengeId) });
+  const challenge = await findChallenge({ id: challengeId }).catch(
+    (error) => {
+      console.error(`db challenge load failure`, error); // TODO: craft some DRY handlers for this type stuff, distinguish between client mode and server mode, etc.
+      throw new error(404, `db load failure for challenge ${challengeId}`);
+    },
+  );
 
   const challengePrompt = cachedReadOnlyClient.fetch(
     `*[_type == "gamePrompt" && _id == $promptId][0]`,
     { promptId: challenge?.promptSanityId },
   ).catch((error) => {
-    console.error(`load failure`, error); // TODO: craft some DRY handlers for this type stuff, distinguish between client mode and server mode, etc.
+    console.error(`cms load failure`, error); // TODO: craft some DRY handlers for this type stuff, distinguish between client mode and server mode, etc.
     throw new error(
       404,
       `cms load failure for prompt ${challenge?.promptSanityId}`,
