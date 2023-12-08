b8e03f260a6b523f8417937cec908f849cf548a9
Brandon Dees <dees@pm.me>
2023-12-07 22:16:20 -0600

    memos for improvements

--- frontend/src/lib/StorageStuff.ts
+++ frontend/src/lib/StorageStuff.ts
@@ -1,6 +1,9 @@
-import localforage from 'localforage';
-import { UndoStack } from '$lib/UndoStack';
-import { choodlePromptKey, choodleUndoKey } from '$lib/Configuration';
+import localforage from "localforage";
+import { UndoStack } from "$lib/UndoStack";
+import { choodlePromptKey, choodleUndoKey } from "$lib/Configuration";
+
+// TODO: wrap undostack in a reactive store so that pages can subscribe to it rather than having to manually refresh it
+// TODO: make sure to monitor storage events to keep undo stack in sync across tabs
 
 export async function clearStorage() {
   await localforage.removeItem(choodleUndoKey);
