c5c30dcf8194cf79d5e3d59358c9169cd197f6f5
Brandon Dees <dees@pm.me>
2023-12-07 23:02:24 -0600

    guess this is gone now

--- frontend/src/routes/+layout.server.ts
+++ frontend/src/routes/+layout.server.ts
@@ -1,10 +1,8 @@
-import { VERCEL_ANALYTICS_ID } from "$env/static/private";
 import { cachedReadOnlyClient } from "$lib/CMSUtils";
 import type { LayoutServerLoad } from "./$types";
 
 export const load: LayoutServerLoad = async ({ locals }) => {
   return {
-    analyticsId: VERCEL_ANALYTICS_ID,
     copy: cachedReadOnlyClient.fetch(
       `*[_type == "choodleWithFriendsCopy"] | order(_createdAt) [0]`,
     ),
