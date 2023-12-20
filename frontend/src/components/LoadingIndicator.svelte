5f3116d6b2656d4f41c8e36269beaf2d3b01a44b
Steven R. Baker <steven@stevenrbaker.com>
2023-12-20 20:33:11 +0100

    RIP Bob Ross.

--- frontend/src/components/LoadingIndicator.svelte
+++ frontend/src/components/LoadingIndicator.svelte
@@ -1,18 +1,11 @@
 <script lang="ts">
   import { loading, loadingOverride, loadingReasons, showLoadingIndicator } from '$lib/store'
   import { fade } from 'svelte/transition'
-  import { onMount } from 'svelte'
-  import { toHTML } from '@portabletext/to-html'
   import { navigating, page } from '$app/stores'
   import { urlFor } from '$lib/PersistedImagesUtils'
   import PixelImage from './PixelImage.svelte'
   import { browser } from '$app/environment'
-
-  export let rotatingMessages: unknown[] = []
-
-  // eslint-disable-next-line no-undef
-  let interval: string | number | NodeJS.Timeout | undefined
-  let messageIndex = -1
+  import interstitialImage from '$lib/assets/interstitial-smiley-l.png'
 
   const logLoadingStateDebug = () => {
     console.log(`loading state`, {
@@ -20,26 +13,12 @@
       navigating: $navigating,
       showLoadingIndicator: $showLoadingIndicator,
       loadingReasons: $loadingReasons,
-      interval,
-      messageIndex,
     })
   }
   $: {
     browser && [$loading, $navigating, $loadingReasons, $loadingOverride, $showLoadingIndicator] &&
       logLoadingStateDebug()
   }
-
-  const messageRotationTimeMs = 6000
-  onMount(() => {
-    if (rotatingMessages?.length > 0) {
-      messageIndex = 0
-      interval = setInterval(() => {
-        messageIndex = (messageIndex + 1) % rotatingMessages.length
-      }, messageRotationTimeMs)
-    }
-
-    return () => clearInterval(interval)
-  })
 </script>
 
 {#if $showLoadingIndicator || $loadingOverride}
@@ -59,20 +38,11 @@
       <div class="group">
         <PixelImage
           class="loading-image"
-          height={263}
-          width={174}
-          src="/choodle-bob-p2.png"
+          height={134}
+          width={161}
+          src={interstitialImage}
           alt="A doodle of the painter Bob Ross, slightly smiling"
         />
-        <div class="messageContainer">
-          {#if messageIndex >= 0 && rotatingMessages?.length > 0}
-            {#key messageIndex}
-              <div class="message" transition:fade={{ duration: 300 }}>
-                {@html toHTML(rotatingMessages[messageIndex])}
-              </div>
-            {/key}
-          {/if}
-        </div>
       </div>
     </div>
   </div>
@@ -96,7 +66,7 @@
     margin: 0;
     padding: 0;
 
-    background: #f7f7f7;
+    background: var(--choodle-yellow);
     overflow: hidden;
   }
 
@@ -124,23 +94,6 @@
     height: 32px;
     flex-shrink: 1;
     margin: 0.42rem 1rem;
-  }
-
-  .messageContainer {
-    display: block;
-    position: relative;
-    width: 100%;
-    height: auto;
-    max-width: 500px;
-    text-align: center;
-    margin-bottom: 6rem;
-  }
-
-  .message {
-    position: absolute;
-    padding: 2rem 3rem;
-    text-align: center;
-    width: 100%;
   }
 
   .not-loading {
